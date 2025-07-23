ec887d7905fa3222e1afda5e5e12f72e5d8127c0
dain <0901ekdls@gmail.com>
2025-07-23 17:10:39 +0900

    feat: @NotBlank, @NotNull 추가

--- src/main/java/com/explorer/gabom/domain/place/dto/PlaceRequest.java
+++ src/main/java/com/explorer/gabom/domain/place/dto/PlaceRequest.java
@@ -2,14 +2,29 @@ package com.explorer.gabom.domain.place.dto;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record PlaceRequest(
 
+	@NotBlank
 	String title,
+
+	@NotBlank
 	String address,
+
+	@NotNull
 	Double lat,
+
+	@NotNull
 	Double lng,
+
+	@NotBlank
 	String content,
+
+	@NotBlank
 	String proofMethod,
+
 	List<Long> imageIds
 
 ) {
