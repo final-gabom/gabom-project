c34e75b8905720806d4f90e2c1e0c44caf35a554
dain <0901ekdls@gmail.com>
2025-07-23 17:17:22 +0900

    feat: PlaceRepository 인터페이스 생성

---
+++ src/main/java/com/explorer/gabom/domain/place/repository/PlaceRepository.java
@@ -0,0 +1,9 @@
+package com.explorer.gabom.domain.place.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.explorer.gabom.domain.place.entity.Place;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+}
