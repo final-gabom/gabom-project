40ae16676268d12efb92edc397a548f88408ff2c
dain <0901ekdls@gmail.com>
2025-07-23 20:52:39 +0900

    feat: 장소 등록 서비스 로직에 사용자 정보 연동

--- src/main/java/com/explorer/gabom/domain/place/service/PlaceService.java
+++ src/main/java/com/explorer/gabom/domain/place/service/PlaceService.java
@@ -6,6 +6,7 @@ import com.explorer.gabom.domain.place.dto.request.PlaceCreateRequest;
 import com.explorer.gabom.domain.place.dto.response.PlaceCreateResponse;
 import com.explorer.gabom.domain.place.entity.Place;
 import com.explorer.gabom.domain.place.repository.PlaceRepository;
+import com.explorer.gabom.domain.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,21 +17,9 @@ public class PlaceService {
 	private final PlaceRepository placeRepository;
 
 	// 탐험 장소 생성
-	public PlaceCreateResponse createPlace(PlaceCreateRequest request /*, User user */) {
-		// TODO: 인증 유저 정보 받아서 연관관계 처리 팔요
-		Place place = Place.builder()
-						   /*.user(user)*/
-						   .title(request.getTitle())
-						   .address(request.getAddress())
-						   .lat(request.getLat())
-						   .lng(request.getLng())
-						   .content(request.getContent())
-						   .proofMethod(request.getProofMethod())
-						   .viewCount(0)
-						   .build();
+	public PlaceCreateResponse createPlace(PlaceCreateRequest request, User user) {
 
-		// TODO: user 설정 필요
-		// TODO: 이미지 파일 연관관계 설정 필요
+		Place place = new Place(request, user);
 
 		Place savedPlace = placeRepository.save(place);
 
