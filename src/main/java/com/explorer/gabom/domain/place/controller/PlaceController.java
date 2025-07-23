f602686e08ef6e995efc42bcb195300ce7f9ed6b
dain <0901ekdls@gmail.com>
2025-07-23 20:52:50 +0900

    feat: 장소 등록 시 @AuthenticationPrincipal로 사용자 정보 받도록 수정

--- src/main/java/com/explorer/gabom/domain/place/controller/PlaceController.java
+++ src/main/java/com/explorer/gabom/domain/place/controller/PlaceController.java
@@ -1,7 +1,7 @@
 package com.explorer.gabom.domain.place.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.explorer.gabom.domain.place.dto.request.PlaceCreateRequest;
 import com.explorer.gabom.domain.place.dto.response.PlaceCreateResponse;
 import com.explorer.gabom.domain.place.service.PlaceService;
+import com.explorer.gabom.domain.user.entity.User;
 import com.explorer.gabom.global.dto.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -24,10 +25,10 @@ public class PlaceController {
 	// 탐험 장소 생성
 	@PostMapping
 	public ResponseEntity<ApiResponse<PlaceCreateResponse>> createPlace(
-		@RequestBody PlaceCreateRequest request /* TODO: , @AuthenticationPrincipal User user */) {
-
-		PlaceCreateResponse response = placeService.createPlace(request /*, user */);
-
-		return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("장소 등록에 성공했습니다.", response));
+		@RequestBody PlaceCreateRequest request,
+		@AuthenticationPrincipal User user
+	) {
+		PlaceCreateResponse response = placeService.createPlace(request, user);
+		return ResponseEntity.ok(ApiResponse.success("장소 등록이 완료되었습니다.", response));
 	}
 }
