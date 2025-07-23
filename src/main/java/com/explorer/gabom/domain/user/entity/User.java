0bd7a9256271548a9b926827a7afce836448063c
천세경 <jay45652@gmail.com>
2025-07-23 20:32:48 +0900

    Feat: titleId -> title
    Resolves: #61

--- src/main/java/com/explorer/gabom/domain/user/entity/User.java
+++ src/main/java/com/explorer/gabom/domain/user/entity/User.java
@@ -57,7 +57,7 @@ public class User extends BaseTimeEntity {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "title_id")
-	private Title titleId;
+	private Title title;
 
 	private String address;
 
