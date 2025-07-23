ab6bd9006891574212a0900952e98b668bdb6527
kimwonjun <monkeydmw@naver.com>
2025-07-23 16:53:49 +0900

    Feat: 활동 로그 타입 value로 targetId가 있는 경우 true, 없는 경우 false 설정

---
+++ src/main/java/com/explorer/gabom/domain/activity/aop/ActivityLoggable.java
@@ -0,0 +1,2 @@
+package com.explorer.gabom.domain.activity.aop;public interface ActivityLoggable {
+}
--- src/main/java/com/explorer/gabom/domain/activity/type/ActivityType.java
+++ src/main/java/com/explorer/gabom/domain/activity/type/ActivityType.java
@@ -1,9 +1,19 @@
 package com.explorer.gabom.domain.activity.type;
 
 public enum ActivityType {
-	MISSION_PROOF_CREATED,
-	PLACE_SHARED,
-	QUEST_COMPLETED,
-	USER_FOLLOWED,
-	TITLE_EARNED
+	MISSION_PROOF_CREATED(true),
+	PLACE_SHARED(true),
+	QUEST_COMPLETED(true),
+	TITLE_EARNED(true),
+	USER_LOGIN(false);
+
+	private final boolean requiredTargetId;
+
+	ActivityType(boolean requiredTargetId) {
+		this.requiredTargetId = requiredTargetId;
+	}
+
+	public boolean getRequiredTargetId() {
+		return requiredTargetId;
+	}
 }
