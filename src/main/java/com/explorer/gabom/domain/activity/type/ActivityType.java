a2dd2b1962cc84752f5c244a883fea23aaf97928
kimwonjun <monkeydmw@naver.com>
2025-07-23 21:37:19 +0900

    Feat: enum ActivityType message 속성 추가

--- src/main/java/com/explorer/gabom/domain/activity/type/ActivityType.java
+++ src/main/java/com/explorer/gabom/domain/activity/type/ActivityType.java
@@ -1,19 +1,24 @@
 package com.explorer.gabom.domain.activity.type;
 
+import lombok.Getter;
+
+@Getter
 public enum ActivityType {
-	MISSION_PROOF_CREATED(true),
-	PLACE_SHARED(true),
-	QUEST_COMPLETED(true),
-	TITLE_EARNED(true),
-	USER_LOGIN(false);
+	MISSION_PROOF_CREATED(true, "인증글을 작성하였습니다."),
+
+	PLACE_SHARED(true, "장소를 등록하였습니다."),
+
+	QUEST_COMPLETED(true, "퀘스트를 달성하였습니다."),
+
+	TITLE_EARNED(true, "칭호를 획득하였습니다."),
+
+	AUTH_LOGIN(false, "로그인하였습니다.");
 
 	private final boolean requiredTargetId;
+	private final String message;
 
-	ActivityType(boolean requiredTargetId) {
+	ActivityType(boolean requiredTargetId, String message) {
 		this.requiredTargetId = requiredTargetId;
-	}
-
-	public boolean getRequiredTargetId() {
-		return requiredTargetId;
+		this.message = message;
 	}
 }
