af481737de5fd37a3145d47411f01d84d02adfed
kimwonjun <monkeydmw@naver.com>
2025-07-23 16:57:08 +0900

    Feat: ActivityLoggable 어노테이션 추가, Target은 METHOD, Retention은 RUNTIME으로 설정

--- src/main/java/com/explorer/gabom/domain/activity/aop/ActivityLoggable.java
+++ src/main/java/com/explorer/gabom/domain/activity/aop/ActivityLoggable.java
@@ -1,2 +1,14 @@
-package com.explorer.gabom.domain.activity.aop;public interface ActivityLoggable {
+package com.explorer.gabom.domain.activity.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.explorer.gabom.domain.activity.type.ActivityType;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActivityLoggable {
+	ActivityType value();
 }
