730ae05e482e011e3829ccfad1523fb1420997b9
dain <0901ekdls@gmail.com>
2025-07-23 16:30:57 +0900

    feat: Place 등록 요청 DTO 생성

---
+++ src/main/java/com/explorer/gabom/domain/place/dto/PlaceRequest.java
@@ -0,0 +1,13 @@
+package com.explorer.gabom.domain.place.dto;
+
+import java.util.List;
+
+public record PlaceRequest (
+	String title,
+	String address,
+	Double lat,
+	Double lng,
+	String content,
+	String proofMethod,
+	List<Long> imageIds
+) {}
