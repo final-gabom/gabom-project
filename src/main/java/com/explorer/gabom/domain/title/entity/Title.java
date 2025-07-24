0b09ac8d9c76f75e5f554757ad3879360c863375
kimwonjun <monkeydmw@naver.com>
2025-07-24 12:49:52 +0900

    Feat: Title Entity @Getter 추가

--- src/main/java/com/explorer/gabom/domain/title/entity/Title.java
+++ src/main/java/com/explorer/gabom/domain/title/entity/Title.java
@@ -7,9 +7,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
 @Entity
 @Table(name = "title")
+@Getter
 public class Title extends BaseTimeEntity {
 
 	@Id
