d71efb933d211dc2899b38e423207a33aeca0be1
gyogyo0212 <gyogyo0212@gmail.com>
2025-07-24 17:38:45 +0900

    Feature: 유저 프로필 수정 기능 구현 (#94)
    * Feature: 프로필 수정 기능 추가
    
    수정 기능 추가
    AttachmentFileRepository 파일 추가
    - 프로필이미지 파일 추가가 필요해서 추가
    
    Resolves: #63
    
    * Refactor: 코드 정렬
    
    Resolves: #63
    
    ---------
    
    Co-authored-by: gyogyo0212 <gyogyo0212@gamil.com>

--- src/main/java/com/explorer/gabom/domain/user/service/UserService.java
+++ src/main/java/com/explorer/gabom/domain/user/service/UserService.java
@@ -1,9 +1,12 @@
 package com.explorer.gabom.domain.user.service;
 
 import com.explorer.gabom.domain.user.dto.UserDto;
+import com.explorer.gabom.domain.user.dto.request.UserUpdateRequest;
 
 public interface UserService {
 	UserDto getUser(Long userId);
+
+	UserDto updateUser(Long userId, UserUpdateRequest updateRequest);
 
 	void deleteUser(Long userId);
 }
