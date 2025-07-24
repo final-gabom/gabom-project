8f3623e3783978ff8435a5e20732b9242393c506
이다인 <0901ekdls@gmail.com>
2025-07-24 20:16:03 +0900

    fix: 탐험 장소 등록/수정 시 객체 user->userId로 변경 (#96)
    * fix: 탐험 장소 등록/수정 시 객체 user->userId로 변경
    
    #95
    
    * feat: 테이블 places -> place로 변경
    
    #95
    
    * feat: PlaceController, PlaceCreateRequest에 @Valid 추가
    
    #95

--- src/main/java/com/explorer/gabom/domain/place/dto/request/PlaceCreateRequest.java
+++ src/main/java/com/explorer/gabom/domain/place/dto/request/PlaceCreateRequest.java
@@ -1,5 +1,7 @@
 package com.explorer.gabom.domain.place.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,10 +9,21 @@ import lombok.Getter;
 @AllArgsConstructor
 public class PlaceCreateRequest {
 
+	@NotBlank(message = "제목은 필수입니다.")
 	private final String title;
+
+	@NotBlank(message = "주소는 필수입니다.")
 	private final String address;
+
+	@NotNull(message = "위도 값은 필수입니다.")
 	private final Double lat;
+
+	@NotNull(message = "경도 값은 필수입니다.")
 	private final Double lng;
+
+	@NotBlank(message = "인증 방법은 필수입니다.")
 	private final String proofMethod;
+
+	@NotBlank(message = "본문은 필수입니다.")
 	private final String content;
 }
