000647259b8f68dbcc39502400afa3291be40547
천세경 <jay45652@gmail.com>
2025-07-24 12:23:00 +0900

    Feat: getUser 기능 구현
    Resolves: #61

---
+++ src/main/java/com/explorer/gabom/domain/user/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.explorer.gabom.domain.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.explorer.gabom.domain.user.dto.UserDto;
+import com.explorer.gabom.domain.user.service.UserService;
+import com.explorer.gabom.global.dto.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+	private final UserService userService;
+
+	@GetMapping("/{userId}")
+	public ResponseEntity<ApiResponse<UserDto>> getUser(@PathVariable Long userId) {
+		UserDto response = userService.getUser(userId);
+		return ResponseEntity.ok(ApiResponse.success("회원 정보 조회를 성공하였습니다.", response));
+	}
+}
