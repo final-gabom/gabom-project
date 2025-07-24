d351071a8364e18204bf06add48d423aa3325724
dain <0901ekdls@gmail.com>
2025-07-24 14:51:18 +0900

    feat: 장소 수정 기능 구현 (PATCH /api/places/{placeId})
    #35

--- src/main/java/com/explorer/gabom/domain/place/controller/PlaceController.java
+++ src/main/java/com/explorer/gabom/domain/place/controller/PlaceController.java
@@ -2,12 +2,15 @@ package com.explorer.gabom.domain.place.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.explorer.gabom.domain.place.dto.request.PlaceCreateRequest;
+import com.explorer.gabom.domain.place.dto.request.PlaceUpdateRequest;
 import com.explorer.gabom.domain.place.dto.response.PlaceCreateResponse;
 import com.explorer.gabom.domain.place.service.PlaceService;
 import com.explorer.gabom.domain.user.entity.User;
@@ -37,7 +40,15 @@ public class PlaceController {
 	// 탐험 장소 상세 조회
 
 	// 탐험 장소 수정
-
+	@PatchMapping("/{placeId}")
+	public ResponseEntity<ApiResponse<Void>> updatePlace(
+		@PathVariable Long placeId,
+		@RequestBody PlaceUpdateRequest request,
+		@AuthenticationPrincipal User user
+	) {
+		placeService.updatePlace(placeId, request, user);
+		return ResponseEntity.ok(ApiResponse.success("장소가 수정되었습니다."));
+	}
 
 	// 탐험 장소 삭제
 }
