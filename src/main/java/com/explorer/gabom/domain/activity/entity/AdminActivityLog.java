6e019d921271502b235eb60cac7d3ac9f76cc0c1
kimwonjun <monkeydmw@naver.com>
2025-07-24 15:33:50 +0900

    Feat: 활동타입을 관리자와 유저를 분리. ADMIN_ 접두어가 붙은 활동은 관리자 활동

---
+++ src/main/java/com/explorer/gabom/domain/activity/entity/AdminActivityLog.java
@@ -0,0 +1,2 @@
+package com.explorer.gabom.domain.activity.entity;public class AdminActivityLog {
+}
--- src/main/java/com/explorer/gabom/domain/activity/type/ActivityType.java
+++ src/main/java/com/explorer/gabom/domain/activity/type/ActivityType.java
@@ -4,16 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum ActivityType {
+
+	// 유저 활동
 	MISSION_PROOF_CREATED(true, "인증글을 작성하였습니다."),
-
 	PLACE_SHARED(true, "장소를 등록하였습니다."),
-
-	QUEST_CREATED(true, "퀘스트를 등록하였습니다."),
 	QUEST_COMPLETED(true, "퀘스트를 달성하였습니다."),
-
 	TITLE_EARNED(true, "칭호를 획득하였습니다."),
+	AUTH_LOGIN(false, "로그인하였습니다."),
 
-	AUTH_LOGIN(false, "로그인하였습니다.");
+	// 관리자 활동
+	ADMIN_QUEST_CREATED(true, "퀘스트를 등록하였습니다.");
 
 	private final boolean requiredTargetId;
 	private final String message;
@@ -21,5 +21,9 @@ public enum ActivityType {
 	ActivityType(boolean requiredTargetId, String message) {
 		this.requiredTargetId = requiredTargetId;
 		this.message = message;
+	}
+
+	public boolean isAdminActivity() {
+		return this.name().startsWith("ADMIN_");
 	}
 }
