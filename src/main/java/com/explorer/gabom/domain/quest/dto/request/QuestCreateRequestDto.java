4b682279d0f710788e7440d0551ceedbc9008f71
kimwonjun <monkeydmw@naver.com>
2025-07-24 13:44:55 +0900

    Feat: QuestCreate Request&ResponseDto 추가

--- src/main/java/com/explorer/gabom/domain/quest/dto/request/QuestCreateRequestDto.java
+++ src/main/java/com/explorer/gabom/domain/quest/dto/request/QuestCreateRequestDto.java
@@ -1,2 +1,34 @@
-package com.explorer.gabom.domain.quest.dto.request;public class QuestCreateRequestDto {
+package com.explorer.gabom.domain.quest.dto.request;
+
+import com.explorer.gabom.domain.quest.type.QuestConditionType;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class QuestCreateRequestDto {
+
+	@NotBlank(message = "제목은 필수입니다.")
+	private String title;
+
+	@NotBlank(message = "설명은 필수입니다.")
+	private String description;
+
+	@NotNull(message = "퀘스트 타입은 필수입니다.")
+	private QuestConditionType questConditionType;
+
+	@NotNull(message = "달성 조건은 필수입니다.")
+	private Integer acquireCondition;
+
+	@NotNull(message = "포인트는 필수입니다.")
+	private Integer rewardPoint;
+
+	@NotNull(message = "경험치는 필수입니다.")
+	private Integer rewardExp;
+
+	@NotNull(message = "보상 칭호 ID는 필수입니다.")
+	private Long rewardTitleId;
 }
--- src/main/java/com/explorer/gabom/domain/quest/dto/response/QuestCreateResponseDto.java
+++ src/main/java/com/explorer/gabom/domain/quest/dto/response/QuestCreateResponseDto.java
@@ -1,2 +1,43 @@
-package com.explorer.gabom.domain.quest.dto.response;public class QuestCreateResponseDto {
+package com.explorer.gabom.domain.quest.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.explorer.gabom.domain.quest.entity.Quest;
+import com.explorer.gabom.domain.quest.type.QuestConditionType;
+import com.explorer.gabom.global.dto.TargetIdentifiable;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class QuestCreateResponseDto implements TargetIdentifiable {
+	private Long questId;
+	private String title;
+	private String description;
+	private QuestConditionType questConditionType;
+	private int acquireCondition;
+	private int rewardPoint;
+	private int rewardExp;
+	private Long rewardTitleId;
+	private LocalDateTime createdAt;
+
+	public static QuestCreateResponseDto toDto(Quest quest) {
+		return QuestCreateResponseDto.builder()
+									 .questId(quest.getId())
+									 .title(quest.getTitle())
+									 .description(quest.getDescription())
+									 .questConditionType(quest.getQuestConditionType())
+									 .acquireCondition(quest.getAcquireCondition())
+									 .rewardPoint(quest.getRewardPoint())
+									 .rewardExp(quest.getRewardExp())
+									 .rewardTitleId(quest.getRewardTitle().getId())
+									 .createdAt(quest.getCreatedAt())
+									 .build();
+	}
+
+	@Override
+	public Long getTargetId() {
+		return this.questId;
+	}
 }
