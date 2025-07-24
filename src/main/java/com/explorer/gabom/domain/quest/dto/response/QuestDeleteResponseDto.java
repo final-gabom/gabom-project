fec2ce98521451df13bd69b163f3385272f5ea59
kimwonjun <monkeydmw@naver.com>
2025-07-24 22:35:40 +0900

    Feat: QuestDeleteResponseDto 추가

--- src/main/java/com/explorer/gabom/domain/quest/dto/response/QuestDeleteResponseDto.java
+++ src/main/java/com/explorer/gabom/domain/quest/dto/response/QuestDeleteResponseDto.java
@@ -1,2 +1,28 @@
-package com.explorer.gabom.domain.quest.dto.response;public class QuestDeleteResponseDto {
+package com.explorer.gabom.domain.quest.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.explorer.gabom.global.dto.TargetIdentifiable;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class QuestDeleteResponseDto implements TargetIdentifiable {
+
+	private Long questId;
+	private LocalDateTime deletedAt;
+
+	public static QuestDeleteResponseDto fromId(Long questId) {
+		return QuestDeleteResponseDto.builder()
+									 .questId(questId)
+									 .deletedAt(LocalDateTime.now())
+									 .build();
+	}
+
+	@Override
+	public Long getTargetId() {
+		return this.questId;
+	}
 }
