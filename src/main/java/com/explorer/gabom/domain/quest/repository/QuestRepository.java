0310f250df3d739f9bf1f16b3da62b3950d96fdb
kimwonjun <monkeydmw@naver.com>
2025-07-24 13:00:42 +0900

    Feat: TITLE_NOT_FOUND ErrorCode 추가

---
+++ src/main/java/com/explorer/gabom/domain/quest/repository/QuestRepository.java
@@ -0,0 +1,4 @@
+package com.explorer.gabom.domain.quest.repository;
+
+public class QuestRepository {
+}
--- src/main/java/com/explorer/gabom/global/exception/ErrorCode.java
+++ src/main/java/com/explorer/gabom/global/exception/ErrorCode.java
@@ -13,9 +13,12 @@ public enum ErrorCode {
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
 
 	// Auth
-	EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT,"이미 등록된 이메일입니다." ),
+	EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 등록된 이메일입니다."),
 
-	NICKNAME_ALREADY_EXISTS(HttpStatus.CONFLICT,"이미 등록된 닉네임입니다." );
+	NICKNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 등록된 닉네임입니다."),
+
+	// Title
+	TITLE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 칭호를 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
