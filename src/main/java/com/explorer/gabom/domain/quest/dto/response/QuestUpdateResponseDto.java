c63ef74dc6d50f20fd08f5c1284d2dd2a08f07a5
kimwonjun <monkeydmw@naver.com>
2025-07-24 20:02:20 +0900

    Feat: QuestUpdate Request&ResponseDto 추가. ResponseDto TargetIdentifiable 상속 후 getTargetId 메서드 구현

--- src/main/java/com/explorer/gabom/domain/quest/dto/response/QuestUpdateResponseDto.java
+++ src/main/java/com/explorer/gabom/domain/quest/dto/response/QuestUpdateResponseDto.java
@@ -1,2 +1,44 @@
-package com.explorer.gabom.domain.quest.dto.response;public class QuestUpdateResponseDto {
+package com.explorer.gabom.domain.quest.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.explorer.gabom.domain.quest.entity.Quest;
+import com.explorer.gabom.domain.quest.type.QuestConditionType;
+import com.explorer.gabom.global.dto.TargetIdentifiable;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class QuestUpdateResponseDto implements TargetIdentifiable {
+
+	private Long questId;
+	private String title;
+	private String description;
+	private QuestConditionType questConditionType;
+	private int acquireCondition;
+	private int rewardPoint;
+	private int rewardExp;
+	private Long rewardTitleId;
+	private LocalDateTime updatedAt;
+
+	public static QuestUpdateResponseDto toDto(Quest quest) {
+		return QuestUpdateResponseDto.builder()
+									 .questId(quest.getId())
+									 .title(quest.getTitle())
+									 .description(quest.getDescription())
+									 .questConditionType(quest.getQuestConditionType())
+									 .acquireCondition(quest.getAcquireCondition())
+									 .rewardPoint(quest.getRewardPoint())
+									 .rewardExp(quest.getRewardExp())
+									 .rewardTitleId(quest.getRewardTitle().getId())
+									 .updatedAt(quest.getUpdatedAt())
+									 .build();
+	}
+
+	@Override
+	public Long getTargetId() {
+		return this.questId;
+	}
 }
