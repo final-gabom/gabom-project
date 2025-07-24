6b95b3ad19afc4486300c93a78e0f589b65880af
kimwonjun <monkeydmw@naver.com>
2025-07-24 20:04:08 +0900

    Feat: QuestService updateQuest 로직 추가

--- src/main/java/com/explorer/gabom/domain/quest/service/QuestServiceImpl.java
+++ src/main/java/com/explorer/gabom/domain/quest/service/QuestServiceImpl.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import com.explorer.gabom.domain.activity.aop.ActivityLoggable;
 import com.explorer.gabom.domain.activity.type.ActivityType;
 import com.explorer.gabom.domain.quest.dto.request.QuestCreateRequestDto;
+import com.explorer.gabom.domain.quest.dto.request.QuestUpdateRequestDto;
 import com.explorer.gabom.domain.quest.dto.response.QuestCreateResponseDto;
+import com.explorer.gabom.domain.quest.dto.response.QuestUpdateResponseDto;
 import com.explorer.gabom.domain.quest.entity.Quest;
 import com.explorer.gabom.domain.quest.repository.QuestRepository;
 import com.explorer.gabom.domain.title.entity.Title;
@@ -43,4 +45,22 @@ public class QuestServiceImpl implements QuestService {
 		Quest saved = questRepository.save(quest);
 		return QuestCreateResponseDto.toDto(saved);
 	}
+
+	@Override
+	@Transactional
+	@ActivityLoggable(ActivityType.ADMIN_QUEST_UPDATED)
+	public QuestUpdateResponseDto updateQuest(Long questId, QuestUpdateRequestDto dto) {
+		Quest quest = questRepository.findById(questId)
+									 .orElseThrow(() -> new CustomException(ErrorCode.QUEST_NOT_FOUND));
+
+		Title rewardTitle = null;
+		if (dto.getRewardTitleId() != null) {
+			rewardTitle = titleRepository.findById(dto.getRewardTitleId())
+										 .orElseThrow(() -> new CustomException(ErrorCode.TITLE_NOT_FOUND));
+		}
+
+		quest.update(dto, rewardTitle);
+		return QuestUpdateResponseDto.toDto(quest);
+	}
+
 }
