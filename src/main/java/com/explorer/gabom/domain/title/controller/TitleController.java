a268abcb1ea3dda9d672a0622df773cd13fdacc6
zzzzdong <jieun8496@icloud.com>
2025-07-25 00:26:20 +0900

    fix: TitleController.java @RequestMapping("/api/admin/titles") -> @RequestMapping("/api") 수정 후 @PostMapping에("/admin/titles") 추가 Resolves: #49

--- src/main/java/com/explorer/gabom/domain/title/controller/TitleController.java
+++ src/main/java/com/explorer/gabom/domain/title/controller/TitleController.java
@@ -16,12 +16,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/admin/titles")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class TitleController {
 	private final TitleService titleService;
 
-	@PostMapping
+	@PostMapping("/admin/titles")
 	// @PreAuthorize("hasRole('ADMIN')") 어차피 동작 안해서 spring security 설정 적용 여부에 따라 수정 예정
 	public ResponseEntity<ApiResponse<TitleCreateResponse>> createTitle(
 		@RequestBody @Valid TitleCreateRequest request
