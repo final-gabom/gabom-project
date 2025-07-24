f61b4ca025d2048b43d28d2e0c343c6edec3e2a6
zzzzdong <jieun8496@icloud.com>
2025-07-24 16:02:45 +0900

    fix: 코드 정렬 및 @Slf4j 로그 추가 Resolves: #68

--- src/main/java/com/explorer/gabom/domain/title/service/TitleService.java
+++ src/main/java/com/explorer/gabom/domain/title/service/TitleService.java
@@ -10,21 +10,26 @@ import com.explorer.gabom.global.exception.BusinessException;
 import com.explorer.gabom.global.exception.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TitleService {
 	private final TitleRepository titleRepository;
 
 	public TitleCreateResponse createTitle(TitleCreateRequest request) {
+		log.info("<칭호등록> 요청 - name: {}, description: {}", request.getName(), request.getDescription());
 		if (titleRepository.existsByName(request.getName())) {
+			log.warn("<칭호등록> 실패 - 중복된 이름: {}", request.getName());
 			throw new BusinessException(ErrorCode.TITLE_DUPLICATED);
 		}
 
 		Title title = new Title(request.getName(), request.getDescription());
 		Title saved = titleRepository.save(title);
+
+		log.info("<칭호등록> 성공 - 등록된 ID: {}", saved.getId());
 		return TitleCreateResponse.from(saved);
 	}
-
 
 }
