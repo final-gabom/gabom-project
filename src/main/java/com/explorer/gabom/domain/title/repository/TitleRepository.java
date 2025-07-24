ba75c78898d6968870e1bdda622b01d3e13ff758
kimwonjun <monkeydmw@naver.com>
2025-07-24 13:01:33 +0900

    Feat: TitleRepository 추가

--- src/main/java/com/explorer/gabom/domain/title/repository/TitleRepository.java
+++ src/main/java/com/explorer/gabom/domain/title/repository/TitleRepository.java
@@ -1,2 +1,8 @@
-package com.explorer.gabom.domain.title.repository;public class TitleRepository {
+package com.explorer.gabom.domain.title.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.explorer.gabom.domain.title.entity.Title;
+
+public interface TitleRepository extends JpaRepository<Title, Long> {
 }
