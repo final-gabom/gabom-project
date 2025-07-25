de174d3dc55a92fedbb24b55b397af9554e81909
Kimjieun <jieun8496@icloud.com>
2025-07-25 21:59:06 +0900

    Feature: 칭호 조회 기능 (#115)
    * exception.ErrorCode 내 Title ErrorCode 추가
    exception.BusinessException 추가
    
    * feat: 칭호 등록 기능 추가
    * BaseTimeEntity.java @Getter 추가
    * Title.java @Getter @NoArgsConstructor 추가 name, description 생성자 추가
    * TitleController.java 생성
    * TitleCreateRequest.java 생성
    * TitleRepository.java 생성
    * TitleResponse.java 생성
    * TitleService.java 생성
    
    * fix: TitleController.java @PreAuthorize 주석 추가
    * TitleCreateRequest.java @NotBlank message 추가
    
    * feat: 칭호 수정 기능 추가
    * Title.java update 메서드 추가
    * TitleController.java 칭호 수정 api 추가
    * TitleService.java updateTitle 메서드 추가
    * TitleUpdateRequest.java 생성
    
    * fix: TitleController.java @PutMapping -> @PatchMapping 변경
    
    * feat: TitleController.java @DeleteMapping 추가
    * TitleService.java deleteTitle 메서드 추가
    
    * fix: TitleResponse.java -> TitleCreateResponse.java 이름 변경
    - TitleCreateResponse.java record -> class 수정
    - TitleCreateRequest.java record -> class 수정
    Resolves: #49
    
    * fix: TitleUpdateResponse.java 생성, 이에 따른 서비스, 컨트롤러 수정
    Resolves: #60
    
    * fix: TitleDeleteResponse.java 생성, 이에 따른 서비스, 컨트롤러 수정
    Resolves: #68
    
    * fix: 코드 정렬 및 @Slf4j 로그 추가
    Resolves: #68
    
    * fix: 코드 정렬 및 @Slf4j 로그 추가
    Resolves: #60
    
    * fix: BusinessException.java 삭제 후 CustomException.jave 사용하도록 수정
    * TitleService.java 엔티티 -> DTO 변환 메서드 from(...) -> toDto() 변경
    Resolves: #49
    
    * fix: BusinessException.java 삭제 후 CustomException.jave 사용하도록 수정
    * TitleService.java 엔티티 -> DTO 변환 메서드명 from() -> toDto() 변경
    Resolves: #60
    
    * fix: ActivityType.java ADMIN_TITLE_CREATED, UPDATED 추가
    * Title.java @LastModifiedDate로 자동 업데이트 되므로 this.updatedAt = LocalDateTime.now() 삭제
    * TitleCreateResponse.java, TitleUpdateResponse.java 활동로그에서 targetId값 가져올 수 있도록 TargetIdentifiable 상속, @Override 추가
    * TitleService.java @Transactional 추가
    * TitleService.java @ActivityLoggable 추가
    *
    Resolves: #60
    
    * fix: Title.java update 메서드 부분 name, description이 각각 null이 아닌 경우에만 update 되도록 조건 추가
    Resolves: #60
    
    * fix: ActivityType.java ADMIN_TITLE_DELETED 추가
    * TitleDeleteResponse.java toDto 메서드 추가 및 implements TargetIdentifiable하여 getTargetId 메서드 추가
    * TitleService.java @ActivityLoggable 추가 및 toDto 메서드 사용하도록 수정
    Resolves: #68
    
    * fix: TitleService.java 로그 추가
    Resolves: #68
    
    * feat: ActivityType.java VIEW_USER_TITLES 추가
    * 컨트롤러, 서비스, 레포지토리 Admin/User 분리
    * UserTitle 엔티티 생성
    * UserTitleController 생성
    * UserTitleRepository 생성
    * UserTitleResponse 생성
    * UserTitleService 생성
    Resolves: #71
    
    * fix: UserTitleService.java @ActivityLoggable 삭제
    * UserTitleResponse.java implements TargetIdentifiable 삭제, getTargetId() 메서드 삭제
    Resolves: #71
    
    ---------
    
    Co-authored-by: kimwonjun1 <monkeydmw@naver.com>

---
+++ src/main/java/com/explorer/gabom/domain/title/entity/UserTitle.java
@@ -0,0 +1,36 @@
+package com.explorer.gabom.domain.title.entity;
+
+import com.explorer.gabom.domain.user.entity.User;
+import com.explorer.gabom.global.entity.BaseTimeEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user_title")
+public class UserTitle extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "title_id")
+	private Title title;
+
+	private boolean isEquipped;
+
+}
