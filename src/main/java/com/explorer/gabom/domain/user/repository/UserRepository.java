bad513cd17eea883a3ca72dd96802e3f326b3303
gyogyo0212 <gyogyo0212@gamil.com>
2025-07-25 15:48:51 +0900

    Feature: 로그인 기능 구현
    Resolves: #40

--- src/main/java/com/explorer/gabom/domain/user/repository/UserRepository.java
+++ src/main/java/com/explorer/gabom/domain/user/repository/UserRepository.java
@@ -1,17 +1,19 @@
 package com.explorer.gabom.domain.user.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.explorer.gabom.domain.user.entity.User;
 import com.explorer.gabom.domain.user.type.UserStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	boolean existsByEmail(String email);
+    Optional<User> findByEmailAndStatus(String email, UserStatus userStatus);
 
-	Optional<User> findByIdAndStatus(Long userId, UserStatus status);
+    boolean existsByEmail(String email);
 
-	boolean existsByNickname(String nickname);
+    Optional<User> findByIdAndStatus(Long userId, UserStatus status);
+
+    boolean existsByNickname(String nickname);
+
 }
