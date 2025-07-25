b3c01527d7ea9e0eb931280decf4e0f5b78d6fe8
dain <0901ekdls@gmail.com>
2025-07-25 20:44:48 +0900

    feat: PlaceRepositoryImpl 동적 업데이트 처리
    #100

--- src/main/java/com/explorer/gabom/domain/place/repository/PlaceRepositoryImpl.java
+++ src/main/java/com/explorer/gabom/domain/place/repository/PlaceRepositoryImpl.java
@@ -3,7 +3,10 @@ package com.explorer.gabom.domain.place.repository;
 import com.explorer.gabom.domain.place.dto.request.PlaceUpdateRequest;
 import com.explorer.gabom.domain.place.entity.Place;
 import com.explorer.gabom.domain.place.entity.QPlace;
+import com.explorer.gabom.global.exception.CustomException;
+import com.explorer.gabom.global.exception.ErrorCode;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.jpa.impl.JPAUpdateClause;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,19 +20,56 @@ public class PlaceRepositoryImpl implements PlaceRepositoryCustom {
 	public Place updatePlace(Long placeId, Long userId, PlaceUpdateRequest request) {
 		QPlace place = QPlace.place;
 
-		long updated = queryFactory.update(place)
-								   .set(place.title, request.getTitle())
-								   .set(place.address, request.getAddress())
-								   .set(place.lat, request.getLat())
-								   .set(place.lng, request.getLng())
-								   .set(place.proofMethod, request.getProofMethod())
-								   .set(place.content, request.getContent())
-								   .where(place.id.eq(placeId).and(place.user.id.eq(userId)))
-								   .execute();
+		// 동적 업데이트
+		JPAUpdateClause update = queryFactory.update(place);
+		int setCount = 0;
 
-		if (updated == 0) return null;
+		if (request.getTitle() != null) {
+			update.set(place.address, request.getAddress());
+			setCount++;
+		}
+		if (request.getAddress() != null) {
+			update.set(place.address, request.getAddress());
+			setCount++;
+		}
+		if (request.getLat() != null) {
+			update.set(place.lat, request.getLat());
+			setCount++;
+		}
+		if (request.getLng() != null) {
+			update.set(place.lng, request.getLng());
+			setCount++;
+		}
+		if (request.getProofMethod() != null) {
+			update.set(place.proofMethod, request.getProofMethod());
+			setCount++;
+		}
+		if (request.getContent() != null) {
+			update.set(place.content, request.getContent());
+			setCount++;
+		}
 
-		return placeRepository.findById(placeId)
-							  .orElseThrow(() -> new IllegalArgumentException("업데이트 후 Place 조회를 실패했습니다."));
+		// 수정할 값이 하나도 없으면 예외
+		if (setCount == 0) {
+			throw new CustomException(ErrorCode.NO_FIELDS_TO_UPDATE);
+		}
+
+		long updated = update
+			.where(
+				place.id.eq(placeId),
+				place.user.id.eq(userId)
+			)
+			.execute();
+
+		// 서비스에서 PLACE_NO_PERMISSION 처리
+		if (updated == 0) {
+			return null;
+		}
+
+		// 최종 상태 재조회 후 반환
+		return queryFactory
+			.selectFrom(place)
+			.where(place.id.eq(placeId))
+			.fetchOne();
 		}
 }
