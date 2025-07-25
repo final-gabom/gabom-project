ca47afcc6d5ba6c7de9741f1224a4d86c1f0aa52
kimwonjun <monkeydmw@naver.com>
2025-07-25 19:32:33 +0900

    Feat: QuestService getQuestById 로직 추가

--- src/main/java/com/explorer/gabom/domain/quest/service/QuestService.java
+++ src/main/java/com/explorer/gabom/domain/quest/service/QuestService.java
@@ -2,8 +2,11 @@ package com.explorer.gabom.domain.quest.service;
 
 import org.springframework.data.domain.Pageable;
 
+import com.explorer.gabom.domain.quest.dto.QuestDto;
 import com.explorer.gabom.domain.quest.dto.response.QuestPage;
 
 public interface QuestService {
 	QuestPage getQuestPage(Pageable pageable);
+
+	QuestDto getQuestById(Long questId);
 }
