9be9d0c93930578a2408e416f191ff45e5659616
gyogyo0212 <gyogyo0212@gamil.com>
2025-07-25 15:08:04 +0900

    Feature: PasswordValidator.java 생성
    - 비밀번호 일치여부를 확인하기위해 PasswordValidator.java생성
    - ErrorCode.java: INCORRECT_PASSWORD추가
    
    Resolves: #40

--- src/main/java/com/explorer/gabom/global/exception/ErrorCode.java
+++ src/main/java/com/explorer/gabom/global/exception/ErrorCode.java
@@ -23,6 +23,9 @@ public enum ErrorCode {
 	INVALID_TOKEN_VALUE(HttpStatus.BAD_REQUEST, "잘못된 토큰 값입니다."),
 	EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 등록된 이메일입니다."),
 	NICKNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 등록된 닉네임입니다."),
+	INCORRECT_PASSWORD(HttpStatus.FORBIDDEN, "비밀번호가 일치하지 않습니다."),
+
+	// File
 	FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "파일을 찾을 수 없습니다."),
 
 	// Place
