efc32e81484989324dd188ceeaa0e31c6f9aa8be
gyogyo0212 <gyogyo0212@gmail.com>
2025-07-28 23:30:48 +0900

    [feature] 내 칭호 변경 기능 구현 (#136)
    * feature: 내 칭호 변경 기능 작성 커밋
    
    Resolves: #67
    
    * Refactor: 내 칭호 변경기능 수정
    
    - 클래스명, 메서드명, 컨트롤러 코드 부분 수정
    
    Resolves: #67
    
    ---------
    
    Co-authored-by: gyogyo0212 <gyogyo0212@gamil.com>

---
+++ src/main/java/com/explorer/gabom/domain/user/dto/request/UpdateMainTitleRequest.java
@@ -0,0 +1,10 @@
+package com.explorer.gabom.domain.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UpdateMainTitleRequest {
+	private final Long titleId;
+}
