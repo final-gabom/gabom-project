4dedc56d8e3f3b1f809ae77d65e4a6ed80186f12
kimwonjun <monkeydmw@naver.com>
2025-07-28 14:29:51 +0900

    Feat: QuestRewardResponse 추가

--- src/main/java/com/explorer/gabom/domain/quest/dto/response/QuestRewardResponse.java
+++ src/main/java/com/explorer/gabom/domain/quest/dto/response/QuestRewardResponse.java
@@ -1,2 +1,22 @@
-package com.explorer.gabom.domain.quest.dto.response;public class QuestRewardResponse {
+package com.explorer.gabom.domain.quest.dto.response;
+
+import com.explorer.gabom.domain.quest.entity.Quest;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class QuestRewardResponse {
+	private int rewardPoint;
+	private int rewardExp;
+	private Long rewardTitleId;
+
+	public static QuestRewardResponse toDto(Quest quest) {
+		return QuestRewardResponse.builder()
+								  .rewardPoint(quest.getRewardPoint())
+								  .rewardExp(quest.getRewardExp())
+								  .rewardTitleId(quest.getRewardTitle() != null ? quest.getRewardTitle().getId() : null)
+								  .build();
+	}
 }
