5a66a114b7ddbc652c512b8dd5a2761f5daeddda
kimwonjun <monkeydmw@naver.com>
2025-07-28 14:28:33 +0900

    Feat: ErrorCode NOT_COMPLETED, REWARD_ALREADY_CLAIMED 추가

--- src/main/java/com/explorer/gabom/global/exception/ErrorCode.java
+++ src/main/java/com/explorer/gabom/global/exception/ErrorCode.java
@@ -47,7 +47,8 @@ public enum ErrorCode {
 
 	// Quest
 	QUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 퀘스트를 찾을 수 없습니다."),
-	;
+	NOT_COMPLETED(HttpStatus.BAD_REQUEST, "퀘스트가 완료되지 않았습니다."),
+	REWARD_ALREADY_CLAIMED(HttpStatus.CONFLICT, "보상을 이미 수령하였습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
