d4d45144b2b2ad6439b760659b44754d92d91fd4
dain <0901ekdls@gmail.com>
2025-07-28 17:50:23 +0900

    feat: PlaceStatus enum에 DELETED 상태 추가 및 soft delete 처리 반영
    #38

--- src/main/java/com/explorer/gabom/domain/place/repository/PlaceRepository.java
+++ src/main/java/com/explorer/gabom/domain/place/repository/PlaceRepository.java
@@ -3,11 +3,13 @@ package com.explorer.gabom.domain.place.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.parameters.P;
 
 import com.explorer.gabom.domain.place.entity.Place;
+import com.explorer.gabom.domain.place.entity.PlaceStatus;
 
 public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceRepositoryCustom {
 
 	Optional<Place> findByIdAndUserId(Long placeId, Long userId);
+
+	Optional<Place> findByIdAndStatusAndDeletedAtIsNull(Long id, PlaceStatus status);
 }
