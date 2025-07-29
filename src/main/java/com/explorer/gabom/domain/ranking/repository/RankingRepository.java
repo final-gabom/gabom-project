3e346721bd2f4e3f96252725237b7efd6c0f7cb6
kimwonjun <monkeydmw@naver.com>
2025-07-29 19:39:54 +0900

    Feat: RankingRepository 추가

--- src/main/java/com/explorer/gabom/domain/ranking/repository/RankingRepository.java
+++ src/main/java/com/explorer/gabom/domain/ranking/repository/RankingRepository.java
@@ -1,2 +1,10 @@
-package com.explorer.gabom.domain.ranking.repository;public class RankingRepository {
+package com.explorer.gabom.domain.ranking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.explorer.gabom.domain.ranking.entity.Ranking;
+
+@Repository
+public interface RankingRepository extends JpaRepository<Ranking, Long> {
 }
