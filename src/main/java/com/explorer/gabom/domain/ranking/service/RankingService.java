709bee6a49f122981755b65b2b058cd984f59795
kimwonjun <monkeydmw@naver.com>
2025-07-29 19:42:11 +0900

    Feat: RankingService getRankingPage 메서드 추가

--- src/main/java/com/explorer/gabom/domain/ranking/service/RankingService.java
+++ src/main/java/com/explorer/gabom/domain/ranking/service/RankingService.java
@@ -1,4 +1,9 @@
 package com.explorer.gabom.domain.ranking.service;
 
+import org.springframework.data.domain.Pageable;
+
+import com.explorer.gabom.domain.ranking.dto.response.RankingPage;
+
 public interface RankingService {
+	RankingPage getRankingPage(Pageable pageable);
 }
