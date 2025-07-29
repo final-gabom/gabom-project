e9062ba20ae15021d21a2acc545b3262690c54d5
gyogyo0212 <gyogyo0212@gamil.com>
2025-07-29 15:20:58 +0900

    refactor: 유저 조회부분 수정
    - 사용자의 탈퇴상태확인하는 코드로 수정하였습니다.
    
    Resolves: #69

--- src/main/java/com/explorer/gabom/domain/user/service/UserBlockService.java
+++ src/main/java/com/explorer/gabom/domain/user/service/UserBlockService.java
@@ -1,5 +1,7 @@
 package com.explorer.gabom.domain.user.service;
 
+import static com.explorer.gabom.global.exception.ErrorCode.*;
+
 import org.springframework.stereotype.Service;
 
 import com.explorer.gabom.domain.user.dto.response.UserBlockResponse;
@@ -7,6 +9,7 @@ import com.explorer.gabom.domain.user.entity.User;
 import com.explorer.gabom.domain.user.entity.UserBlock;
 import com.explorer.gabom.domain.user.repository.UserBlockRepository;
 import com.explorer.gabom.domain.user.repository.UserRepository;
+import com.explorer.gabom.domain.user.type.UserStatus;
 import com.explorer.gabom.global.exception.CustomException;
 import com.explorer.gabom.global.exception.ErrorCode;
 
@@ -21,17 +24,18 @@ public class UserBlockService {
 
 	// 차단 기능 (blocker 차단하는 사람, blocked 차단 당하는 사람)
 	@Transactional
-	public UserBlockResponse blockUser(Long blockerId, Long blockedId){
-		if (blockerId.equals(blockedId)) {
-			throw new CustomException(ErrorCode.CANNOT_BLOCK_SELF);
+	public UserBlockResponse blockUser(Long userId, Long blockedId){
+		if (userId.equals(blockedId)) {
+			throw new CustomException(CANNOT_BLOCK_SELF);
 		}
-		User blocker = userRepository.findById(blockerId)
-									 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-		User blocked = userRepository.findById(blockedId)
-									 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+		User blocker = userRepository.findByIdAndStatus(userId, UserStatus.ACTIVE)
+									 .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+		User blocked = userRepository.findByIdAndStatus(blockedId, UserStatus.ACTIVE)
+									 .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
 		boolean alreadyBlocked = userBlockRepository.existsByBlockerAndBlocked(blocker, blocked);
 		if (alreadyBlocked) {
-			throw new CustomException(ErrorCode.ALREADY_BLOCKED_USER);
+			throw new CustomException(ALREADY_BLOCKED_USER);
 		}
 			UserBlock userBlock = new UserBlock(blocker, blocked);
 			userBlockRepository.save(userBlock);
