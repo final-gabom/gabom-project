e9e8517ad950e57893ea0cde30b1ce4ae4767874
kimwonjun <monkeydmw@naver.com>
2025-07-29 19:41:11 +0900

    Feat: RankingDto, RankingPage 추가

--- src/main/java/com/explorer/gabom/domain/ranking/dto/response/RankingPage.java
+++ src/main/java/com/explorer/gabom/domain/ranking/dto/response/RankingPage.java
@@ -1,2 +1,30 @@
-package com.explorer.gabom.domain.ranking.dto.response;public class RankingPage {
+package com.explorer.gabom.domain.ranking.dto.response;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.explorer.gabom.domain.ranking.dto.RankingDto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class RankingPage {
+	private List<RankingDto> content;
+	private int page;
+	private int size;
+	private long totalElements;
+	private int totalPages;
+
+	public static RankingPage toDto(Page<RankingDto> pageData) {
+		return RankingPage.builder()
+						  .content(pageData.getContent())
+						  .page(pageData.getNumber())
+						  .size(pageData.getSize())
+						  .totalElements(pageData.getTotalElements())
+						  .totalPages(pageData.getTotalPages())
+						  .build();
+	}
 }
