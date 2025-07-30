ae2085c29a4be3b6be21945b5ea0acc02ceebd6d
gyogyo0212 <gyogyo0212@gmail.com>
2025-07-30 11:29:06 +0900

    [Feature] #50 이메일 인증코드 전송 기능  (#160)
    * refactor: 이메일 인증 기능 yml,gradle설정 추가
    
    - 이메일 인증구현에 필요한 설정들을 추가
    
    Resolves: #50
    
    * feature: 이메일 인증 기능 yml,gradle설정 추가
    
    - 이메일 인증 기능 구현에 필요한 redis 설정 추가
    
    Resolves: #50
    
    * feature: 이메일 인증 기능 (redisService 작성)
    
    - 이메일 인증 코드를 저장,조회,삭제 기능을 함
    
    Resolves: #50
    
    * feature: 이메일 인증 기능 (EmailAuthService작성)
    
    - 이메일 인증 코드를 발송,인증코드 저장,인증코드 생성
    
    Resolves: #50
    
    * feature: 이메일 인증 기능
    
    - email을 직접 받다가 dto 객체로 변경중
    
    Resolves: #50
    
    * refactor: 테스트코드 오류
    
    -	@Mock
     	private TitleRepository titleRepository;
     	추가
     	생성자에 추가 titleRepository
    
    Resolves: #50
    
    * feature: 이메일 인증코드 전송 기능 구현
    
    - 이메일 인증코드 이메일로 오는지 확인
    - JavaMailSenderConfig.java 작성
    - 예외처리 추가
    - 이메일 인증 포스트맨 테스트시 토큰을 필요로 하는중
    
    Resolves: #50
    
    * refactor: RedisService.java 파일명 변경
    
    - EmailCodeStorageService.java로 변경하였습니다.
    
    Resolves: #50
    
    * refactor: EmailCodeStorageService.java 파일명 변경
    
    - EmailCodeStorage.java로 변경하였습니다.
    
    Resolves: #50
    
    ---------
    
    Co-authored-by: gyogyo0212 <gyogyo0212@gamil.com>

---
+++ src/main/java/com/explorer/gabom/global/config/JavaMailSenderConfig.java
@@ -0,0 +1,46 @@
+package com.explorer.gabom.global.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class JavaMailSenderConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private Integer port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(host);
+        javaMailSender.setPort(port);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);  // 오타 수정됨
+        javaMailSender.setJavaMailProperties(getMailProperties());
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        // 필요에 따라 추가 설정 (예: SSL 설정)
+        return properties;
+    }
+}
