208b4f65eab05848d5a789b01b40fe3975cb6b79
dain <0901ekdls@gmail.com>
2025-07-30 15:13:34 +0900

    feat: 탐험 중복 여부 확인 로직 추가
    #39

---
+++ src/main/java/com/explorer/gabom/domain/Exploration/service/ExplorationService.java
@@ -0,0 +1,32 @@
+package com.explorer.gabom.domain.Exploration.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.explorer.gabom.domain.Exploration.dto.request.ExplorationStartRequest;
+import com.explorer.gabom.domain.Exploration.dto.response.ExplorationStartResponse;
+import com.explorer.gabom.domain.Exploration.repository.ExplorationRepository;
+import com.explorer.gabom.domain.place.repository.PlaceRepository;
+import com.explorer.gabom.domain.user.repository.UserRepository;
+import com.explorer.gabom.global.exception.CustomException;
+import com.explorer.gabom.global.exception.ErrorCode;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ExplorationService {
+
+	private final UserRepository userRepository;
+	private final PlaceRepository placeRepository;
+	private final ExplorationRepository explorationRepository;
+
+	@Transactional
+	public ExplorationStartResponse startExploration(Long userId, Long placeId, ExplorationStartRequest request) {
+		if (explorationRepository.existsByUserIdAndPlaceIdAndEndAtAfter(userId, placeId, LocalDateTime.now())) {
+			throw new CustomException(ErrorCode.ALREADY_STARTED_EXPLORATION);
+		}
+	}
+}
