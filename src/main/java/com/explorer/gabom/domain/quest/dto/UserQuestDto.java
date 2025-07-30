cf5938bb7409262b7e3d7e7d2f7f3dc5c99b313c
kimwonjun <monkeydmw@naver.com>
2025-07-30 15:00:57 +0900

    Feat: UserQuestDto 추가

--- src/main/java/com/explorer/gabom/domain/quest/dto/UserQuestDto.java
+++ src/main/java/com/explorer/gabom/domain/quest/dto/UserQuestDto.java
@@ -1,4 +1,33 @@
-package com.explorer.gabom.domain.quest.dto.response;
+package com.explorer.gabom.domain.quest.dto;
 
-public class UserQuestProgressResponse {
+import java.time.LocalDateTime;
+
+import com.explorer.gabom.domain.quest.entity.UserQuest;
+import com.explorer.gabom.domain.quest.type.ProgressStatus;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserQuestDto {
+	private Long userQuestId;
+	private String questTitle;
+	private ProgressStatus progressStatus;
+	private int progressCount;
+	private int acquireCondition;
+	private boolean rewardClaimed;
+	private LocalDateTime completedAt;
+
+	public static UserQuestDto toDto(UserQuest userQuest) {
+		return UserQuestDto.builder()
+						   .userQuestId(userQuest.getId())
+						   .questTitle(userQuest.getQuest().getTitle())
+						   .progressStatus(userQuest.getProgressStatus())
+						   .progressCount(userQuest.getProgressCount())
+						   .acquireCondition(userQuest.getQuest().getAcquireCondition())
+						   .rewardClaimed(userQuest.isRewardClaimed())
+						   .completedAt(userQuest.getCompletedAt())
+						   .build();
+	}
 }
