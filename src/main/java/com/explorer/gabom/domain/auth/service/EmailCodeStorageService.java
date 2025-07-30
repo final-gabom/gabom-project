ea8a0c28f3c0d07e957b168aec113982a086ce78
gyogyo0212 <gyogyo0212@gamil.com>
2025-07-30 15:17:08 +0900

    feature: EmailCodeStorageService.java 작성
    - 인증 완료 여부 확인, 인증 완료 상태 저장 추가
    
    Resolves: #48

--- src/main/java/com/explorer/gabom/domain/auth/service/EmailCodeStorageService.java
+++ src/main/java/com/explorer/gabom/domain/auth/service/EmailCodeStorageService.java
@@ -2,6 +2,7 @@ package com.explorer.gabom.domain.auth.service;
 
 import java.util.concurrent.TimeUnit;
 
+import com.explorer.gabom.domain.auth.dto.request.EmailCodeVerifyRequest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class EmailCodeStorage {
+public class EmailCodeStorageService {
 	public final RedisTemplate<String, String> redisTemplate;
 
 	// 인증 코드 저장
@@ -30,5 +31,17 @@ public class EmailCodeStorage {
 	public void deleteEmailAuthCode(EmailRequest request) {
 		String email = request.getEmail();
 		redisTemplate.delete("EMAIL_AUTH:" + email);
+	}
+
+	// 인증 완료 여부 확인
+	public boolean isEmailVerified(EmailCodeVerifyRequest request) {
+		String email = request.getEmail();
+		String checkEmail = redisTemplate.opsForValue().get("EMAIL_VERIFIED:"+ email);
+		return "true".equals(checkEmail);
+	}
+	// 인증 완료 상태 저장
+	public void setEmailVerified(EmailCodeVerifyRequest request, long expirationSeconds) {
+		String email = request.getEmail();
+		redisTemplate.opsForValue().set("EMAIL_VERIFIED:" + email,"true",expirationSeconds, TimeUnit.SECONDS);
 	}
 }
