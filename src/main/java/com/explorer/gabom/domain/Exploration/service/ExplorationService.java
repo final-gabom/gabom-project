384cd16f475c3e7012853d251f4a77cd1ce47188
dain <0901ekdls@gmail.com>
2025-07-30 15:31:16 +0900

    feat: 탐험 시작 시 유저 및 장소 조회 로직 추가
    #39

--- src/main/java/com/explorer/gabom/domain/Exploration/service/ExplorationService.java
+++ src/main/java/com/explorer/gabom/domain/Exploration/service/ExplorationService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.explorer.gabom.domain.Exploration.dto.request.ExplorationStartRequest;
 import com.explorer.gabom.domain.Exploration.dto.response.ExplorationStartResponse;
 import com.explorer.gabom.domain.Exploration.repository.ExplorationRepository;
+import com.explorer.gabom.domain.place.entity.Place;
 import com.explorer.gabom.domain.place.repository.PlaceRepository;
+import com.explorer.gabom.domain.user.entity.User;
 import com.explorer.gabom.domain.user.repository.UserRepository;
 import com.explorer.gabom.global.exception.CustomException;
 import com.explorer.gabom.global.exception.ErrorCode;
@@ -28,5 +30,8 @@ public class ExplorationService {
 		if (explorationRepository.existsByUserIdAndPlaceIdAndEndAtAfter(userId, placeId, LocalDateTime.now())) {
 			throw new CustomException(ErrorCode.ALREADY_STARTED_EXPLORATION);
 		}
+
+		User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+		Place place = placeRepository.findById(placeId).orElseThrow(() -> new CustomException(ErrorCode.PLACE_NOT_FOUND));
 	}
 }
