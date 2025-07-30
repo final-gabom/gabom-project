0a328b07189c68bd733327fc55c7d1722b8e8bd0
kimwonjun1 <monkeydmw@naver.com>
2025-07-30 11:06:34 +0900

    Refactor: 페이지 공통 응답 객체 추가 (#162)
    * Feat: 랭킹 정렬 조건 exp로 변경
    
    * Feat: 페이징 공통 응답 객체 추가
    
    * Feat: Service PageResponse 사용

--- src/main/java/com/explorer/gabom/domain/quest/service/QuestService.java
+++ src/main/java/com/explorer/gabom/domain/quest/service/QuestService.java
@@ -3,10 +3,10 @@ package com.explorer.gabom.domain.quest.service;
 import org.springframework.data.domain.Pageable;
 
 import com.explorer.gabom.domain.quest.dto.QuestDto;
-import com.explorer.gabom.domain.quest.dto.response.QuestPage;
+import com.explorer.gabom.global.dto.PageResponse;
 
 public interface QuestService {
-	QuestPage getQuestPage(Pageable pageable);
+	PageResponse<QuestDto> getQuestPage(Pageable pageable);
 
 	QuestDto getQuestById(Long questId);
 }
--- src/main/java/com/explorer/gabom/domain/ranking/controller/RankingController.java
+++ src/main/java/com/explorer/gabom/domain/ranking/controller/RankingController.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.explorer.gabom.domain.ranking.dto.response.RankingPage;
+import com.explorer.gabom.domain.ranking.dto.RankingDto;
 import com.explorer.gabom.domain.ranking.service.RankingService;
 import com.explorer.gabom.global.dto.ApiResponse;
+import com.explorer.gabom.global.dto.PageResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,11 +24,10 @@ public class RankingController {
 	private final RankingService rankingService;
 
 	@GetMapping
-	public ResponseEntity<ApiResponse<RankingPage>> getRankingPage(
-		@PageableDefault(page = 0, size = 10, sort = {"level",
-			"exp"}, direction = Sort.Direction.DESC) Pageable pageable
+	public ResponseEntity<ApiResponse<PageResponse<RankingDto>>> getRankingPage(
+		@PageableDefault(page = 0, size = 10, sort = "exp", direction = Sort.Direction.DESC) Pageable pageable
 	) {
-		RankingPage response = rankingService.getRankingPage(pageable);
+		PageResponse<RankingDto> response = rankingService.getRankingPage(pageable);
 		return ResponseEntity.status(HttpStatus.OK)
 							 .body(ApiResponse.success("전체 랭킹 조회를 완료하였습니다.", response));
 	}
