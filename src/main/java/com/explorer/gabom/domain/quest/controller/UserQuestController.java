261c2891dcf873c861cd8d0420201a6e303c36a3
kimwonjun <monkeydmw@naver.com>
2025-07-30 15:03:01 +0900

    Feat: 유저-퀘스트 조회 API 추가

--- src/main/java/com/explorer/gabom/domain/quest/controller/UserQuestController.java
+++ src/main/java/com/explorer/gabom/domain/quest/controller/UserQuestController.java
@@ -1,16 +1,22 @@
 package com.explorer.gabom.domain.quest.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.explorer.gabom.domain.quest.dto.UserQuestDto;
 import com.explorer.gabom.domain.quest.dto.response.QuestRewardResponse;
 import com.explorer.gabom.domain.quest.service.UserQuestService;
 import com.explorer.gabom.global.dto.ApiResponse;
+import com.explorer.gabom.global.dto.PageResponse;
 import com.explorer.gabom.global.security.userdetails.CustomUserDetails;
 
 import lombok.RequiredArgsConstructor;
@@ -30,5 +36,16 @@ public class UserQuestController {
 		QuestRewardResponse response = userQuestService.claimReward(userDetails.getUserId(), userQuestId);
 		return ResponseEntity.status(HttpStatus.OK)
 							 .body(ApiResponse.success("퀘스트 보상이 지급되었습니다.", response));
+	}
+
+	@GetMapping
+	public ResponseEntity<ApiResponse<PageResponse<UserQuestDto>>> getProgress(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PageableDefault(page = 0, size = 10, sort = {"progressStatus",
+			"completedAt"}, direction = Sort.Direction.ASC) Pageable pageable
+	) {
+		PageResponse<UserQuestDto> response = userQuestService.getProgress(
+			userDetails.getUserId(), pageable);
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("유저의 퀘스트 조회를 성공했습니다.", response));
 	}
 }
