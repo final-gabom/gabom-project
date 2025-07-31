f2fdd1dde8d3b556a7210e0046a6823a10b6ee12
dain <0901ekdls@gmail.com>
2025-07-31 13:54:01 +0900

    feat: 현재 진행 중인 탐험 조회를 위한 Repository 메서드 추가
    #65

--- src/main/java/com/explorer/gabom/domain/exploration/repository/ExplorationRepository.java
+++ src/main/java/com/explorer/gabom/domain/exploration/repository/ExplorationRepository.java
@@ -1,6 +1,7 @@
 package com.explorer.gabom.domain.exploration.repository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ import com.explorer.gabom.domain.exploration.entity.Exploration;
 public interface ExplorationRepository extends JpaRepository<Exploration, Long> {
 
 	boolean existsByUserIdAndPlaceIdAndEndAtAfter(Long userId, Long placeId, LocalDateTime now);
+
+	Optional<Exploration> findTopByUserIdAndEndAtAfterOrderByStartedAtDesc(Long userId, LocalDateTime now);
 }
