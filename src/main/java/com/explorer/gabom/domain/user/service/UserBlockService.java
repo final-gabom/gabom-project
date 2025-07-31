6e7135c7823fcb250697061e2558bbb0b93747ac
gyogyo0212 <gyogyo0212@gamil.com>
2025-07-31 11:42:00 +0900

    Feature: UserBlockService.java 코드추가
    - 차단 해제 작성
    
    Resolves: #69

--- src/main/java/com/explorer/gabom/domain/user/service/UserBlockService.java
+++ src/main/java/com/explorer/gabom/domain/user/service/UserBlockService.java
@@ -1,9 +1,5 @@
 package com.explorer.gabom.domain.user.service;
 
-import static com.explorer.gabom.global.exception.ErrorCode.*;
-
-import org.springframework.stereotype.Service;
-
 import com.explorer.gabom.domain.user.dto.response.UserBlockResponse;
 import com.explorer.gabom.domain.user.entity.User;
 import com.explorer.gabom.domain.user.entity.UserBlock;
@@ -11,34 +7,51 @@ import com.explorer.gabom.domain.user.repository.UserBlockRepository;
 import com.explorer.gabom.domain.user.repository.UserRepository;
 import com.explorer.gabom.domain.user.type.UserStatus;
 import com.explorer.gabom.global.exception.CustomException;
-import com.explorer.gabom.global.exception.ErrorCode;
-
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.explorer.gabom.global.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
 public class UserBlockService {
-	private final UserRepository userRepository;
-	private final UserBlockRepository userBlockRepository;
+    private final UserRepository userRepository;
+    private final UserBlockRepository userBlockRepository;
 
-	// 차단 기능 (blocker 차단하는 사람, blocked 차단 당하는 사람)
-	@Transactional
-	public UserBlockResponse blockUser(Long userId, Long blockedId){
-		if (userId.equals(blockedId)) {
-			throw new CustomException(CANNOT_BLOCK_SELF);
-		}
-		User blocker = userRepository.findByIdAndStatus(userId, UserStatus.ACTIVE)
-									 .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
-		User blocked = userRepository.findByIdAndStatus(blockedId, UserStatus.ACTIVE)
-									 .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+    // 차단 기능 (blocker 차단하는 사람, blocked 차단 당하는 사람)
+    @Transactional
+    public UserBlockResponse blockUser(Long userId, Long blockedId) {
+        if (userId.equals(blockedId)) {
+            throw new CustomException(CANNOT_BLOCK_SELF);
+        }
+        User blocker = userRepository.findByIdAndStatus(userId, UserStatus.ACTIVE)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+        User blocked = userRepository.findByIdAndStatus(blockedId, UserStatus.ACTIVE)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
 
-		boolean alreadyBlocked = userBlockRepository.existsByBlockerAndBlocked(blocker, blocked);
-		if (alreadyBlocked) {
-			throw new CustomException(ALREADY_BLOCKED_USER);
-		}
-			UserBlock userBlock = new UserBlock(blocker, blocked);
-			userBlockRepository.save(userBlock);
-			return new UserBlockResponse(blocker.getId(), blocked.getId());
-	}
+        boolean alreadyBlocked = userBlockRepository.existsByBlockerAndBlocked(blocker, blocked);
+        if (alreadyBlocked) {
+            throw new CustomException(ALREADY_BLOCKED_USER);
+        }
+        UserBlock userBlock = new UserBlock(blocker, blocked);
+        userBlockRepository.save(userBlock);
+        return new UserBlockResponse(blocker.getId(), blocked.getId());
+    }
+    // 차단 해제
+    @Transactional
+    public void unblockUser(Long userId, Long blockedId) {
+        if (userId.equals(blockedId)) {
+            throw new CustomException(CANNOT_BLOCK_SELF);
+        }
+        User blocker = userRepository.findByIdAndStatus(userId, UserStatus.ACTIVE)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+        User blocked = userRepository.findByIdAndStatus(blockedId, UserStatus.ACTIVE)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+        UserBlock userBlock = userBlockRepository.findByBlockerAndBlocked(blocker, blocked)
+                .orElseThrow(() -> new CustomException(NOT_BLOCKED_USER));
+
+        userBlockRepository.delete(userBlock);
+    }
 }
