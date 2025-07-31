9fa75b10f190d50877195df273b3d3e7d250c051
gyogyo0212 <gyogyo0212@gamil.com>
2025-07-31 16:47:02 +0900

    feature: user.java
    - 비밀번호 변경 시 사용할 메서드 작성
    
    Resolves: #44

--- src/main/java/com/explorer/gabom/domain/user/entity/User.java
+++ src/main/java/com/explorer/gabom/domain/user/entity/User.java
@@ -1,33 +1,19 @@
 package com.explorer.gabom.domain.user.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.hibernate.annotations.SQLDelete;
-
+import com.explorer.gabom.domain.file.entity.AttachmentFile;
 import com.explorer.gabom.domain.title.entity.Title;
 import com.explorer.gabom.domain.title.entity.UserTitle;
 import com.explorer.gabom.domain.user.type.UserRole;
 import com.explorer.gabom.domain.user.type.UserStatus;
 import com.explorer.gabom.global.entity.BaseTimeEntity;
-import com.explorer.gabom.domain.file.entity.AttachmentFile;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -36,108 +22,110 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 public class User extends BaseTimeEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(unique = true, nullable = false)
-	private String email;
+    @Column(unique = true, nullable = false)
+    private String email;
 
-	@Column(nullable = false)
-	private String password;
+    @Column(nullable = false)
+    private String password;
 
-	@Column(unique = true, nullable = false)
-	private String nickname;
+    @Column(unique = true, nullable = false)
+    private String nickname;
 
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private UserRole userRole;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole userRole;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "profile_img_id")
-	private AttachmentFile profileImg;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_img_id")
+    private AttachmentFile profileImg;
 
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private UserStatus status;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "title_id")
-	private Title title;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "title_id")
+    private Title title;
 
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<UserTitle> userTitles = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<UserTitle> userTitles = new ArrayList<>();
 
-	private String address;
+    private String address;
 
-	private Double lat;
+    private Double lat;
 
-	private Double lng;
+    private Double lng;
 
-	@Column(nullable = false)
-	private Integer point;
+    @Column(nullable = false)
+    private Integer point;
 
-	@Column(nullable = false)
-	private Integer level;
+    @Column(nullable = false)
+    private Integer level;
 
-	@Column(nullable = false)
-	private Integer exp;
+    @Column(nullable = false)
+    private Integer exp;
 
-	@Builder
-	public User(Long id, String email, String password, String nickname, UserRole userRole) {
-		this.id = id;
-		this.email = email;
-		this.password = password;
-		this.nickname = nickname;
-		this.userRole = userRole;
-		this.status = UserStatus.ACTIVE;
-		this.point = 0;
-		this.level = 1;
-		this.exp = 0;
-	}
+    @Builder
+    public User(Long id, String email, String password, String nickname, UserRole userRole) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.userRole = userRole;
+        this.status = UserStatus.ACTIVE;
+        this.point = 0;
+        this.level = 1;
+        this.exp = 0;
+    }
 
-	public void addPoint(int point) {
-		this.point += point;
-	}
+    public void addPoint(int point) {
+        this.point += point;
+    }
 
-	public void addExp(int exp) {
-		this.exp += exp;
-	}
+    public void addExp(int exp) {
+        this.exp += exp;
+    }
 
-	public void addTitle(Title title) {
-		boolean alreadyHas = userTitles.stream()
-									   .anyMatch(userTitle -> userTitle.getTitle().equals(title));
-		if (!alreadyHas) {
-			userTitles.add(new UserTitle(this, title));
-		}
-	}
+    public void addTitle(Title title) {
+        boolean alreadyHas = userTitles.stream()
+                .anyMatch(userTitle -> userTitle.getTitle().equals(title));
+        if (!alreadyHas) {
+            userTitles.add(new UserTitle(this, title));
+        }
+    }
 
-	public void updateNickname(String nickname) {
-		this.nickname = nickname;
-	}
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-	public void updateProfileImg(AttachmentFile profileImg) {
-		this.profileImg = profileImg;
-	}
+    public void updateProfileImg(AttachmentFile profileImg) {
+        this.profileImg = profileImg;
+    }
 
-	public void updateAddress(String address) {
-		this.address = address;
-	}
+    public void updateAddress(String address) {
+        this.address = address;
+    }
 
-	public void updateLat(Double lat) {
-		this.lat = lat;
-	}
+    public void updateLat(Double lat) {
+        this.lat = lat;
+    }
 
-	public void updateLng(Double lng) {
-		this.lng = lng;
-	}
+    public void updateLng(Double lng) {
+        this.lng = lng;
+    }
 
-	public void setTitle(Title newTitle) {
-		this.title = newTitle;
-	}
+    public void setTitle(Title newTitle) {
+        this.title = newTitle;
+    }
 
-	public void updatePassword(String encodedNewPassword) {
-		this.password = encodedNewPassword;
-	}
+    public void updatePassword(String encodedNewPassword) {
+        this.password = encodedNewPassword;
+    }
+
+    public void changePassword(String encodedPassword) {this.password = encodedPassword;}
 }
 
