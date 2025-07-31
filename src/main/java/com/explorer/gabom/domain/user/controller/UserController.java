ec4ce2b303215e6698f534a5e40098b21750c47b
gyogyo0212 <gyogyo0212@gamil.com>
2025-07-31 21:28:43 +0900

    Refactor: UserController.java
     - todo 삭제
    
    Resolves: #186

--- src/main/java/com/explorer/gabom/domain/user/controller/UserController.java
+++ src/main/java/com/explorer/gabom/domain/user/controller/UserController.java
@@ -35,7 +35,6 @@ public class UserController {
         return ResponseEntity.ok(ApiResponse.success("회원 정보 조회를 성공하였습니다.", response));
     }
 
-    //todo : AuthenticationPrinciple 에서 유저 아이디 가져오도록 수정
     // User 프로필 수정
     @PatchMapping("/me")
     public ResponseEntity<ApiResponse<UserDto>> updateUser(
