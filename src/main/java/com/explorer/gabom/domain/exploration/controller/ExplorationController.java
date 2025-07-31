1f78958727ff28d82fc4732c252016a3efc18075
dain <0901ekdls@gmail.com>
2025-07-31 14:15:19 +0900

    feat: 현재 탐험 중인 장소 조회 API 추가
    #65

--- src/main/java/com/explorer/gabom/domain/exploration/controller/ExplorationController.java
+++ src/main/java/com/explorer/gabom/domain/exploration/controller/ExplorationController.java
@@ -1,7 +1,9 @@
 package com.explorer.gabom.domain.exploration.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.explorer.gabom.domain.exploration.dto.request.ExplorationStartRequest;
+import com.explorer.gabom.domain.exploration.dto.response.ExplorationCurrentResponse;
 import com.explorer.gabom.domain.exploration.dto.response.ExplorationStartResponse;
 import com.explorer.gabom.domain.exploration.service.ExplorationService;
 import com.explorer.gabom.global.dto.ApiResponse;
@@ -23,6 +26,7 @@ public class ExplorationController {
 
 	private final ExplorationService explorationService;
 
+	// 탐험 시작
 	@PostMapping("/{placeId}/start")
 	public ResponseEntity<ApiResponse<ExplorationStartResponse>> startExploration(
 		@PathVariable Long placeId,
@@ -32,5 +36,16 @@ public class ExplorationController {
 		Long userId = userDetails.getUserId();
 		ExplorationStartResponse response = explorationService.startExploration(userId, placeId, request);
 		return ResponseEntity.ok(ApiResponse.success("탐험이 시작되었습니다.", response));
+	}
+
+	// 탐험 중인 장소 조회
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/current")
+	public ResponseEntity<ApiResponse<ExplorationCurrentResponse>> getCurrentExploration(
+		@AuthenticationPrincipal CustomUserDetails userDetails
+	) {
+		Long userId = userDetails.getUserId();
+		ExplorationCurrentResponse response = explorationService.getCurrentExploration(userDetails.getUserId());
+		return ResponseEntity.ok(ApiResponse.success("현재 탐험 중인 장소 조회에 성공했습니다.", response));
 	}
 }
