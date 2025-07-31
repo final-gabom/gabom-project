f8a0726e5140c5a0486e8b32a153d7d82466d848
천세경 <jay45652@gmail.com>
2025-08-01 00:00:36 +0900

    Chore: 사용하지 않는 import문 제거
    Resolves:#43

--- src/main/java/com/explorer/gabom/domain/user/controller/UserController.java
+++ src/main/java/com/explorer/gabom/domain/user/controller/UserController.java
@@ -23,9 +23,6 @@ import com.explorer.gabom.global.security.userdetails.CustomUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -35,50 +32,50 @@ public class UserController {
 
 	private final UserService userService;
 
-    @GetMapping("/me")
-    public ResponseEntity<ApiResponse<UserDto>> getMyProfile(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        UserDto userDto = userService.getUser(userDetails.getUser());
-        return ResponseEntity.ok(ApiResponse.success("내 프로필 조회를 성공하였습니다.", userDto));
-    }
+	@GetMapping("/me")
+	public ResponseEntity<ApiResponse<UserDto>> getMyProfile(@AuthenticationPrincipal CustomUserDetails userDetails) {
+		UserDto userDto = userService.getUser(userDetails.getUser());
+		return ResponseEntity.ok(ApiResponse.success("내 프로필 조회를 성공하였습니다.", userDto));
+	}
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<ApiResponse<UserDto>> getUser(@PathVariable Long userId) {
-        UserDto response = userService.getUser(userId);
-        return ResponseEntity.ok(ApiResponse.success("회원 정보 조회를 성공하였습니다.", response));
-    }
+	@GetMapping("/{userId}")
+	public ResponseEntity<ApiResponse<UserDto>> getUser(@PathVariable Long userId) {
+		UserDto response = userService.getUser(userId);
+		return ResponseEntity.ok(ApiResponse.success("회원 정보 조회를 성공하였습니다.", response));
+	}
 
-    // User 프로필 수정
-    @PatchMapping("/me")
-    public ResponseEntity<ApiResponse<UserDto>> updateUser(
-            @AuthenticationPrincipal CustomUserDetails userDetails,
-            @RequestBody UserUpdateRequest updateRequest) {
-        User user = userDetails.getUser();
-        UserDto updateResponse = userService.updateUser(user, updateRequest);
-        return ResponseEntity.ok(ApiResponse.success("프로필 수정을 완료하였습니다.", updateResponse));
-    }
+	// User 프로필 수정
+	@PatchMapping("/me")
+	public ResponseEntity<ApiResponse<UserDto>> updateUser(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@RequestBody UserUpdateRequest updateRequest) {
+		User user = userDetails.getUser();
+		UserDto updateResponse = userService.updateUser(user, updateRequest);
+		return ResponseEntity.ok(ApiResponse.success("프로필 수정을 완료하였습니다.", updateResponse));
+	}
 
-    @DeleteMapping("/me")
-    public ResponseEntity<ApiResponse<Void>> deleteUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        User user = userDetails.getUser();
-        userService.deleteUser(user);
-        return ResponseEntity.ok(ApiResponse.success("회원 탈퇴가 완료되었습니다."));
-    }
+	@DeleteMapping("/me")
+	public ResponseEntity<ApiResponse<Void>> deleteUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+		User user = userDetails.getUser();
+		userService.deleteUser(user);
+		return ResponseEntity.ok(ApiResponse.success("회원 탈퇴가 완료되었습니다."));
+	}
 
-    @PatchMapping("/me/titles")
-    public ResponseEntity<ApiResponse<UpdateMainTitleResponse>> updateTitle(
-            @AuthenticationPrincipal CustomUserDetails userDetails,
-            @RequestBody UpdateMainTitleRequest request) {
+	@PatchMapping("/me/titles")
+	public ResponseEntity<ApiResponse<UpdateMainTitleResponse>> updateTitle(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@RequestBody UpdateMainTitleRequest request) {
 
-        UpdateMainTitleResponse response = userService.updateMainTitle(userDetails.getUser(), request.getTitleId());
-        return ResponseEntity.ok(ApiResponse.success("칭호를 변경하였습니다.", response));
-    }
+		UpdateMainTitleResponse response = userService.updateMainTitle(userDetails.getUser(), request.getTitleId());
+		return ResponseEntity.ok(ApiResponse.success("칭호를 변경하였습니다.", response));
+	}
 
-    @PatchMapping("/me/password")
-    public ResponseEntity<ApiResponse<Void>> updatePassword(@AuthenticationPrincipal CustomUserDetails userDetails,
-                                                            @RequestBody @Valid PasswordUpdateRequest passwordUpdateRequest) {
-        log.info("비밀번호 변경 요청: userId={}", userDetails.getUser().getId());
-        userService.updatePassword(userDetails.getUser(), passwordUpdateRequest);
-        return ResponseEntity.ok(ApiResponse.success("비밀번호가 성공적으로 변경되었습니다."));
+	@PatchMapping("/me/password")
+	public ResponseEntity<ApiResponse<Void>> updatePassword(@AuthenticationPrincipal CustomUserDetails userDetails,
+															@RequestBody @Valid PasswordUpdateRequest passwordUpdateRequest) {
+		log.info("비밀번호 변경 요청: userId={}", userDetails.getUser().getId());
+		userService.updatePassword(userDetails.getUser(), passwordUpdateRequest);
+		return ResponseEntity.ok(ApiResponse.success("비밀번호가 성공적으로 변경되었습니다."));
 
-    }
+	}
 }
