ae7954b68f5867779d8d2145e3f345b251226409
dain <0901ekdls@gmail.com>
2025-07-31 15:17:20 +0900

    import문 최적화
    #65

--- src/main/java/com/explorer/gabom/domain/exploration/controller/ExplorationController.java
+++ src/main/java/com/explorer/gabom/domain/exploration/controller/ExplorationController.java
@@ -1,7 +1,6 @@
 package com.explorer.gabom.domain.exploration.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
