17d23c35e1be33c6171aae1977896a1a60cce873
gyogyo0212 <gyogyo0212@gamil.com>
2025-07-31 16:50:33 +0900

    feature: EmailAuthController.java 코드 추가
    - 비밀번호 재설정을 위한 인증 코드 검증 + 비밀번호 재설정
    
    Resolves: #44

--- src/main/java/com/explorer/gabom/domain/auth/controller/EmailAuthController.java
+++ src/main/java/com/explorer/gabom/domain/auth/controller/EmailAuthController.java
@@ -3,6 +3,7 @@ package com.explorer.gabom.domain.auth.controller;
 import com.explorer.gabom.domain.auth.dto.request.EmailCodeVerifyRequest;
 import com.explorer.gabom.domain.auth.dto.request.EmailRequest;
 import com.explorer.gabom.domain.auth.dto.request.PasswordResetRequest;
+import com.explorer.gabom.domain.auth.dto.request.PasswordResetVerifyRequest;
 import com.explorer.gabom.domain.auth.service.EmailAuthService;
 import com.explorer.gabom.domain.auth.service.PasswordResetService;
 import com.explorer.gabom.global.dto.ApiResponse;
@@ -21,13 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailAuthController {
     private final EmailAuthService emailAuthService;
     private final PasswordResetService passwordResetService;
-
+    // 이메일 인증 코드 전송
     @PostMapping("/email/request")
     public ResponseEntity<ApiResponse<Void>> requestEmail(@RequestBody EmailRequest request) {
         emailAuthService.sendAuthCode(request);
         return ResponseEntity.ok(ApiResponse.success("인증 코드를 이메일로 전송했습니다."));
     }
-
+    // 이메일 인증 코드 검증
     @PostMapping("/email/verify")
     public ResponseEntity<ApiResponse<Void>> verifiedEmail(@RequestBody EmailCodeVerifyRequest request) {
         emailAuthService.verifyAuthCode((request));
@@ -38,5 +39,11 @@ public class EmailAuthController {
     public ResponseEntity<ApiResponse<Void>> passwordResetRequestEmail(@RequestBody PasswordResetRequest request) {
         passwordResetService.sendResetCode(request);
         return ResponseEntity.ok(ApiResponse.success("비밀번호 재설정 인증 코드를 이메일로 전송했습니다."));
+    }
+    // 비밀번호 재설정을 위한 인증 코드 검증 + 비밀번호 재설정
+    @PostMapping("password-reset/verify")
+    public ResponseEntity<ApiResponse<Void>> passwordResetVerifiedEmail(@RequestBody PasswordResetVerifyRequest resetVerifyRequest) {
+        passwordResetService.verifiedResetCode(resetVerifyRequest);
+        return ResponseEntity.ok(ApiResponse.success("비밀번호 재설정이 완료 되었습니다. "));
     }
 }
