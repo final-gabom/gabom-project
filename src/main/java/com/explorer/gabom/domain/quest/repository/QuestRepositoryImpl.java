3728ce6cba3ad325e567d11c302ac91cf880ead0
kimwonjun <monkeydmw@naver.com>
2025-08-01 18:35:49 +0900

    Feat: UserQuestDto Quest -> UserQuestDto 변환 메서드 toDto 추가

---
+++ src/main/java/com/explorer/gabom/domain/quest/repository/QuestRepositoryImpl.java
@@ -0,0 +1,2 @@
+package com.explorer.gabom.domain.quest.repository;public class QuestRepositoryImpl {
+}
---
+++ src/main/java/com/explorer/gabom/domain/quest/repository/UserQuestRepositoryCustom.java
@@ -0,0 +1,2 @@
+package com.explorer.gabom.domain.quest.repository;public interface UserQuestRepositoryCustom {
+}
