ca02e2a9f85c6038e14be911342ce8e9b8a7c54b
gyogyo0212 <gyogyo0212@gmail.com>
2025-08-01 19:15:10 +0900

    [Test]#196 회원가입 테스트코드 작성 (#197)
    * Refactor: AuthServiceTest.java
    
    테스트코드 작성
    
    Resolves: #193
    
    * Refactor: AuthControllerTest.java
    
    테스트코드 작성
    
    Resolves: #193
    
    ---------
    
    Co-authored-by: gyogyo0212 <gyogyo0212@gamil.com>

---
+++ src/test/java/com/explorer/gabom/domain/auth/controller/AuthControllerTest.java
@@ -0,0 +1,59 @@
+package com.explorer.gabom.domain.auth.controller;
+
+import com.explorer.gabom.domain.auth.dto.request.SignupRequest;
+import com.explorer.gabom.domain.auth.dto.response.SignupResponse;
+import com.explorer.gabom.domain.auth.service.AuthService;
+import com.explorer.gabom.domain.user.type.UserRole;
+import com.explorer.gabom.global.security.jwt.JwtProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private AuthService authService;
+    @MockBean
+    private JwtProvider jwtProvider;
+
+    @Test
+    void 회원가입_성공() throws Exception {
+        // given
+        SignupRequest request = new SignupRequest(
+                "test@example.com",
+                "nickname",
+                "Password123!",
+                UserRole.USER
+        );
+        SignupResponse response = new SignupResponse(1L);
+
+        when(authService.signup(any(SignupRequest.class))).thenReturn(response);
+
+        // when & then
+        mockMvc.perform(post("/api/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.id").value(1));
+    }
+}
---
+++ src/test/java/com/explorer/gabom/domain/auth/service/AuthServiceTest.java
@@ -0,0 +1,125 @@
+package com.explorer.gabom.domain.auth.service;
+
+import com.explorer.gabom.domain.auth.dto.request.EmailCodeVerifyRequest;
+import com.explorer.gabom.domain.auth.dto.request.SignupRequest;
+import com.explorer.gabom.domain.auth.dto.response.SignupResponse;
+import com.explorer.gabom.domain.user.entity.User;
+import com.explorer.gabom.domain.user.repository.UserRepository;
+import com.explorer.gabom.domain.user.type.UserRole;
+import com.explorer.gabom.global.exception.CustomException;
+import com.explorer.gabom.global.exception.ErrorCode;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+
+    @InjectMocks
+    private AuthService authService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private EmailCodeStorageService emailCodeStorageService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void 회원가입_성공() {
+        // given
+        SignupRequest request = new SignupRequest(
+                "test@example.com",
+                "nickname",
+                "password123!",
+                UserRole.USER
+        );
+
+        EmailCodeVerifyRequest verifyRequest = EmailCodeVerifyRequest.onlyEmail(request.getEmail());
+
+        when(userRepository.existsByEmail(request.getEmail())).thenReturn(false);
+        when(emailCodeStorageService.isEmailVerified(any(EmailCodeVerifyRequest.class))).thenReturn(true);
+        when(userRepository.existsByNickname(request.getNickname())).thenReturn(false);
+        when(passwordEncoder.encode(request.getPassword())).thenReturn("encodedPassword");
+
+        User fakeUser = User.builder()
+                .id(1L)
+                .email("test@example.com")
+                .password("encodedPassword")
+                .nickname("nickname")
+                .userRole(UserRole.USER)
+                .build();
+
+        when(userRepository.save(any(User.class))).thenReturn(fakeUser);
+
+        // when
+        SignupResponse response = authService.signup(request);
+
+        // then
+        assertEquals(1L, response.getId()); // id만 있으니 id 값으로 체크
+    }
+
+    @Test
+    void 회원가입_실패_이메일중복() {
+        // given
+        SignupRequest request = new SignupRequest(
+                "test@example.com", "nickname", "password123!", UserRole.USER
+        );
+
+        when(userRepository.existsByEmail(request.getEmail())).thenReturn(true);
+
+        // when & then
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            authService.signup(request);
+        });
+
+        assertEquals(ErrorCode.EMAIL_ALREADY_EXISTS, exception.getErrorCode());
+    }
+
+    @Test
+    void 회원가입_실패_이메일인증안됨() {
+        // given
+        SignupRequest request = new SignupRequest(
+                "test@example.com", "nickname", "password123!", UserRole.USER
+        );
+
+        when(userRepository.existsByEmail(request.getEmail())).thenReturn(false);
+        when(emailCodeStorageService.isEmailVerified(any(EmailCodeVerifyRequest.class))).thenReturn(false);
+
+        // when & then
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            authService.signup(request);
+        });
+
+        assertEquals(ErrorCode.EMAIL_NOT_VERIFIED, exception.getErrorCode());
+    }
+
+    @Test
+    void 회원가입_실패_닉네임중복() {
+        // given
+        SignupRequest request = new SignupRequest(
+                "test@example.com", "nickname", "password123!", UserRole.USER
+        );
+
+        when(userRepository.existsByEmail(request.getEmail())).thenReturn(false);
+        when(emailCodeStorageService.isEmailVerified(any(EmailCodeVerifyRequest.class))).thenReturn(true);
+        when(userRepository.existsByNickname(request.getNickname())).thenReturn(true);
+
+        // when & then
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            authService.signup(request);
+        });
+
+        assertEquals(ErrorCode.NICKNAME_ALREADY_EXISTS, exception.getErrorCode());
+    }
+}
