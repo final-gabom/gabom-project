b338fe955f4ee541524e909780687851f6a40333
이다인 <0901ekdls@gmail.com>
2025-08-06 01:28:11 +0900

    Feat: 탐험 장소 Swagger (#241)
    * feat: PlaceControllerDocs 추가
    
    #239
    
    * feat: PlaceRecommendationControllerDocs 추가
    
    #239
    
    * feat: Swagger 문서화를 위한 Place 관련 DTO에 @Schema 어노테이션 적용
    
    #239
    
    * fix: Place 중복 어노테이션 삭제
    
    #239
    
    * refactor: Dto 파일명 리펙터링
    
    - PlaceDetailResponseDto -> PlaceDetailResponse
    - PlaceRecommendationRequestDto -> PlaceRecommendationRequest
    
    #239

--- src/main/java/com/explorer/gabom/domain/place/dto/request/PlaceUpdateRequest.java
+++ src/main/java/com/explorer/gabom/domain/place/dto/request/PlaceUpdateRequest.java
@@ -4,17 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "장소 수정 요청 정보")
 public class PlaceUpdateRequest {
 
+	@Schema(description = "장소 제목", example = "진짜 맛있는 국밥집")
 	private String title;
+
+	@Schema(description = "장소 주소", example = "부산 광안리")
 	private String address;
+
+	@Schema(description = "위도", example = "35.1530")
 	private Double lat;
+
+	@Schema(description = "경도", example = "129.1180")
 	private Double lng;
+
+	@Schema(description = "장소 설명", example = "쫄면이 진짜 맛있는 집이에요.")
 	private String content;
+
+	@Schema(description = "인증 방법", example = "사진 인증")
 	private String proofMethod;
 }
