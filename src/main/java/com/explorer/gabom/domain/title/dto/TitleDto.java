751d2fd56506c1988378ed59b84191f135719179
Kimjieun <jieun8496@icloud.com>
2025-08-05 22:01:55 +0900

    Feat: Title 기능 Swagger 작성 (#224)
    * Feat: Title 기능 Swagger 작성
    
    * Fix: 스웨거 설명 보완, 리퀘스트 설명은 리퀘스트 안에서 하도록 옮김

--- src/main/java/com/explorer/gabom/domain/title/dto/TitleDto.java
+++ src/main/java/com/explorer/gabom/domain/title/dto/TitleDto.java
@@ -2,6 +2,7 @@ package com.explorer.gabom.domain.title.dto;
 
 import com.explorer.gabom.domain.title.entity.Title;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,8 +10,11 @@ import lombok.Getter;
 @Getter
 public class TitleDto {
 
+	@Schema(description = "칭호 ID")
 	private final Long id;
+	@Schema(description = "칭호 이름")
 	private final String name;
+	@Schema(description = "칭호 설명")
 	private final String description;
 
 	public static TitleDto toDto(Title title) {
--- src/main/java/com/explorer/gabom/domain/title/dto/response/UserTitleResponse.java
+++ src/main/java/com/explorer/gabom/domain/title/dto/response/UserTitleResponse.java
@@ -3,6 +3,7 @@ package com.explorer.gabom.domain.title.dto.response;
 import com.explorer.gabom.domain.title.entity.Title;
 import com.explorer.gabom.domain.title.entity.UserTitle;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserTitleResponse {
+
+	@Schema(description = "조회한 칭호 ID")
 	private Long id;
+	@Schema(description = "조회한 칭호 이름")
 	private String name;
+	@Schema(description = "조회한 칭호 설명")
 	private String description;
 
 	public static UserTitleResponse toDto(UserTitle userTitle) {
