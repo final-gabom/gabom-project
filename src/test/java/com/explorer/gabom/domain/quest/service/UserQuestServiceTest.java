3758297f9b4ea6494c802a46064241a8a8d0c2de
kimwonjun <monkeydmw@naver.com>
2025-08-05 12:42:12 +0900

    UserQuestServiceTest @ExtendWith(MockitoExtension.class) 추가

--- src/test/java/com/explorer/gabom/domain/quest/service/UserQuestServiceTest.java
+++ src/test/java/com/explorer/gabom/domain/quest/service/UserQuestServiceTest.java
@@ -10,9 +10,10 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -32,6 +33,7 @@ import com.explorer.gabom.global.dto.PageResponse;
 import com.explorer.gabom.global.exception.CustomException;
 import com.explorer.gabom.global.exception.ErrorCode;
 
+@ExtendWith(MockitoExtension.class)
 class UserQuestServiceTest {
 
 	private static final Long USER_ID = 1L;
@@ -54,8 +56,6 @@ class UserQuestServiceTest {
 
 	@BeforeEach
 	void setUp() {
-		MockitoAnnotations.openMocks(this);
-
 		pageable = PageRequest.of(0, 10);
 		user = mock(User.class);
 		quest = mock(Quest.class);
