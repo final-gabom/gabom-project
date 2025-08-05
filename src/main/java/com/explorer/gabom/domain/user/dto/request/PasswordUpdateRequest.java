868674b218c19c532cd910ac10820e86b92d92ca
gyogyo0212 <gyogyo0212@gamil.com>
2025-08-06 02:17:27 +0900

    Test: PasswordUpdateRequest.java
    - Schema 어노테이션 작성
    
    Resolves: #240

--- src/main/java/com/explorer/gabom/domain/user/dto/request/PasswordUpdateRequest.java
+++ src/main/java/com/explorer/gabom/domain/user/dto/request/PasswordUpdateRequest.java
@@ -1,13 +1,17 @@
 package com.explorer.gabom.domain.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class PasswordUpdateRequest {
 
+	@Schema(description = "기존 비밀번호", example = "oldPassword123!")
 	@NotBlank(message = "기존 비밀번호를 입력해주세요")
 	private String oldPassword;
+
+	@Schema(description = "새 비밀번호", example = "newPassword456!")
 	@NotBlank(message = "새 비밀번호를 입력해주세요")
 	private String newPassword;
 }
