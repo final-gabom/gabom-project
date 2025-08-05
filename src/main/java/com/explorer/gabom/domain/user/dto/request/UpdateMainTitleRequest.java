47a18c8f1ddfc8d6feb2803b521eaff61f92a9b9
gyogyo0212 <gyogyo0212@gamil.com>
2025-08-06 02:11:27 +0900

    Test: UpdateMainTitleRequest.java
    - Schema 어노테이션 작성
    
    Resolves: #240

--- src/main/java/com/explorer/gabom/domain/user/dto/request/UpdateMainTitleRequest.java
+++ src/main/java/com/explorer/gabom/domain/user/dto/request/UpdateMainTitleRequest.java
@@ -1,10 +1,12 @@
 package com.explorer.gabom.domain.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UpdateMainTitleRequest {
+	@Schema(description = "변경할 칭호 ID", example = "1")
 	private final Long titleId;
 }
