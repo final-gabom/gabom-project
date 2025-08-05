b3143af1e9436e5b2a86a86d66bdf6991e8fe750
GyeongSe <126084941+GyeongSe99@users.noreply.github.com>
2025-08-05 15:27:24 +0900

    Test: #216 authController 통합 테스트 구현  (#221)
    * Test: AuthController 통합 테스트 코드 추가
    
    Resolves: #216
    
    * Test: AuthController 통합 테스트 코드 추가
    
    Resolves: #216
    
    * Test: AuthController 통합 테스트 코드에 보호된 리소스 접근 성공 (Bearer 토큰) 추가
    
    Resolves: #216

--- src/main/java/com/explorer/gabom/global/config/SecurityConfig.java
+++ src/main/java/com/explorer/gabom/global/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
 			)
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(
-					"/api/auth/**, ",
+					"/api/auth/**",
 					"/v3/api-docs/**",
 					"/swagger-ui/**",
 					"/swagger-ui.html",
