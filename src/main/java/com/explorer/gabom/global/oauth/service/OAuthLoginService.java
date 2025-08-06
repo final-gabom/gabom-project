484e86fd47f2ada7cf079739884dd5ce437d0c02
gyogyo0212 <gyogyo0212@gamil.com>
2025-08-06 16:20:51 +0900

    Feature: OAuthLoginService.java작성
    Resolves: #248

--- src/main/java/com/explorer/gabom/global/oauth/service/OAuthLoginService.java
+++ src/main/java/com/explorer/gabom/global/oauth/service/OAuthLoginService.java
@@ -1,5 +1,7 @@
 package com.explorer.gabom.global.oauth.service;
 
-public class OAuthLoginService {
+import com.explorer.gabom.global.oauth.dto.SocialLoginResponse;
+
+public interface  OAuthLoginService {
     SocialLoginResponse login(String accessToken);
 }
