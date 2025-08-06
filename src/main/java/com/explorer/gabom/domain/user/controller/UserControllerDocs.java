96f8449a152d4a7b797bc61e83c5d2669d7ee880
gyogyo0212 <gyogyo0212@gamil.com>
2025-08-06 10:35:22 +0900

    Test: UseControllerDocs.java
    - Swagger 작성완료
    
    Resolves: #240

--- src/main/java/com/explorer/gabom/domain/user/controller/UserControllerDocs.java
+++ src/main/java/com/explorer/gabom/domain/user/controller/UserControllerDocs.java
@@ -1,13 +1,10 @@
 package com.explorer.gabom.domain.user.controller;
 
 
-import com.explorer.gabom.domain.user.dto.UserDto;
 import com.explorer.gabom.domain.user.dto.request.PasswordUpdateRequest;
 import com.explorer.gabom.domain.user.dto.request.UpdateMainTitleRequest;
 import com.explorer.gabom.domain.user.dto.request.UserUpdateRequest;
-import com.explorer.gabom.domain.user.dto.response.UpdateMainTitleResponse;
 import com.explorer.gabom.global.security.userdetails.CustomUserDetails;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -32,29 +29,49 @@ public interface UserControllerDocs {
             @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails userDetails);
 
     @Operation(
-            summary = "회원 정보 조회")
+            summary = "회원 정보 조회",
+            description = "다른 사용자의 프로필 정보를 조횝합니다."
+    )
     @ApiResponse(responseCode = "200", description = "회원 정보를 성공적으로 조회했습니다.")
+    @ApiResponse(responseCode = "404", description = "해당 유저를 찾을 수 없습니다.")
     ResponseEntity<?> getUser(@Parameter(description = "회원 ID", example = "1") @PathVariable Long userId);
 
-    @Operation(summary = "프로필 수정")
+    @Operation(
+            summary = "프로필 수정",
+            description = "현재 로그인된 사용자의 프로필 정보를 수정합니다."
+    )
     @ApiResponse(responseCode = "200", description = "프로필 수정을 성공했습니다.")
+    @ApiResponse(responseCode = "409", description = "이미 등록된 닉네임입니다.")
+    @ApiResponse(responseCode = "404", description = "파일을 찾을 수 없습니다.")
     ResponseEntity<?> updateUser(
             @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestBody UserUpdateRequest updateRequest);
 
-    @Operation(summary = "회원 탈퇴")
+    @Operation(
+            summary = "회원 탈퇴",
+            description = "현재 로그인된 사용자의 정보를 삭제합니다."
+    )
     @ApiResponse(responseCode = "200", description = "회원 탈퇴를 성공했습니다.")
     ResponseEntity<?> deleteUser(
             @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails userDetails);
 
-    @Operation(summary = "메인 칭호 변경")
+    @Operation(
+            summary = "메인 칭호 변경",
+            description = "현재 로그인된 사용자의 메인 칭호를 변경합니다."
+    )
     @ApiResponse(responseCode = "200", description = "칭호를 성공적으로 변경했습니다.")
+    @ApiResponse(responseCode = "404", description = "해당 칭호를 찾을 수 없습니다.")
+    @ApiResponse(responseCode = "404", description = "해당 유저를 찾을 수 없습니다.")
     ResponseEntity<?> updateTitle(
             @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestBody UpdateMainTitleRequest request);
 
-    @Operation(summary = "비밀번호 변경")
+    @Operation(
+            summary = "비밀번호 변경",
+            description = "현재 로그인된 사용자의 비밀번호를 변경합니다."
+    )
     @ApiResponse(responseCode = "200", description = "비밀번호 변경을 성공했습니다.")
+    @ApiResponse(responseCode = "404", description = "해당 유저를 찾을 수 없습니다.")
     ResponseEntity<?> updatePassword(
             @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails userDetails,
             @Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest);
