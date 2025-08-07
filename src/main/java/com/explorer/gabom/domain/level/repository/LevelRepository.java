67965d5f55c500290d38b4b4d54415debc92d8d9
kimwonjun <monkeydmw@naver.com>
2025-08-07 21:51:41 +0900

    Feat: 경험치에 따라 레벨 계산 메서드 추가

--- src/main/java/com/explorer/gabom/domain/level/repository/LevelRepository.java
+++ src/main/java/com/explorer/gabom/domain/level/repository/LevelRepository.java
@@ -1,2 +1,11 @@
-package com.explorer.gabom.domain.level.repository;public class LevelRepository {
+package com.explorer.gabom.domain.level.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.explorer.gabom.domain.level.entity.Level;
+
+public interface LevelRepository extends JpaRepository<Level, Integer> {
+	List<Level> findAllByOrderByRequiredExpAsc();
 }
--- src/main/java/com/explorer/gabom/domain/level/service/LevelService.java
+++ src/main/java/com/explorer/gabom/domain/level/service/LevelService.java
@@ -1,4 +1,32 @@
 package com.explorer.gabom.domain.level.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.explorer.gabom.domain.level.entity.Level;
+import com.explorer.gabom.domain.level.repository.LevelRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class LevelService {
+
+	private final LevelRepository levelRepository;
+
+	public int calculateLevel(int currentExp) {
+		List<Level> levels = levelRepository.findAllByOrderByRequiredExpAsc();
+		int userLevel = 1;
+
+		for (Level level : levels) {
+			if (currentExp >= level.getRequiredExp()) {
+				userLevel = level.getLevel();
+			} else {
+				break;
+			}
+		}
+
+		return userLevel;
+	}
 }
