68314b3d90813d72a97db3aaa31465d7ea2d0062
gyogyo0212 <gyogyo0212@gamil.com>
2025-08-07 17:25:50 +0900

    Feature: SocialLoginController.java
    - redirectToKakao 매서드 작성
      (카카오로그인페이지로 이동)
    
    Resolves: #265

--- src/main/java/com/explorer/gabom/domain/auth/controller/SocialLoginController.java
+++ src/main/java/com/explorer/gabom/domain/auth/controller/SocialLoginController.java
@@ -5,13 +5,14 @@ import com.explorer.gabom.global.oauth.dto.request.SocialLoginRequest;
 import com.explorer.gabom.global.oauth.dto.response.SocialLoginResponse;
 import com.explorer.gabom.global.oauth.service.SocialLoginServiceFactory;
 import com.explorer.gabom.global.oauth.service.SocialOAuthLoginService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +21,22 @@ public class SocialLoginController {
 
     private final SocialLoginServiceFactory socialLoginServiceFactory;
 
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String redirectUri;
+
+    // 카카오 로그인 페이지로 이동 (리다이렉트)
+    @GetMapping("/kakao")
+    public void redirectToKakao(HttpServletResponse response) throws IOException {
+        String kakaoAuthUrl = "https://kauth.kakao.com/oauth/authorize" +
+                "?client_id=" + clientId +
+                "&redirect_uri=" + redirectUri +
+                "&response_type=code";
+
+        response.sendRedirect(kakaoAuthUrl);
+    }
     @PostMapping("/social-login")
     public ResponseEntity<ApiResponse<SocialLoginResponse>>socialLogin(
             @RequestBody @Valid SocialLoginRequest request) {
