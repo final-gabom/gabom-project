aa299eebadda57b86d2e75ad00fe5616fe7032ea
kimwonjun <monkeydmw@naver.com>
2025-08-07 21:50:35 +0900

    Feat: Level Entity 추가

--- src/main/java/com/explorer/gabom/domain/level/entity/Level.java
+++ src/main/java/com/explorer/gabom/domain/level/entity/Level.java
@@ -1,4 +1,21 @@
 package com.explorer.gabom.domain.level.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "level")
 public class Level {
+
+	@Id
+	private int level;
+
+	@Column(nullable = false)
+	private int requiredExp;
 }
