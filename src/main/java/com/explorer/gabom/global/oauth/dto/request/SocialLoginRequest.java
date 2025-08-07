a0cdcb8887f19fbf6c01679403450ed3aa805010
gyogyo0212 <gyogyo0212@gamil.com>
2025-08-07 10:44:08 +0900

    Feature: SocialLoginRequest.java 작성
    Resolves: #248

---
+++ src/main/java/com/explorer/gabom/global/oauth/dto/request/SocialLoginRequest.java
@@ -0,0 +1,15 @@
+package com.explorer.gabom.global.oauth.dto.request;
+
+import com.explorer.gabom.global.oauth.type.OAuthProvider;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class SocialLoginRequest {
+
+    @NotNull(message = "소셜 로그인 제공자를 입력해주세요.")
+    private OAuthProvider provider;
+
+    @NotBlank(message = "인가 코드를 입력해주세요.")
+    private String code;
+}
+
