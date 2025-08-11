45a3adaf7d03d0b75aeb7c3aee69a0553d692357
kimwonjun <monkeydmw@naver.com>
2025-08-11 19:55:18 +0900

    Feat: .sql 실행 API 추가

--- src/main/java/com/explorer/gabom/domain/sql/controller/SqlExecutionController.java
+++ src/main/java/com/explorer/gabom/domain/sql/controller/SqlExecutionController.java
@@ -1,4 +1,26 @@
 package com.explorer.gabom.domain.sql.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.explorer.gabom.domain.sql.service.SqlExecutionService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/admin/sql")
+@RequiredArgsConstructor
 public class SqlExecutionController {
+
+	private final SqlExecutionService sqlExecutionService;
+
+	@PostMapping("/run")
+	public ResponseEntity<Void> runSqlFile(@RequestParam String file) {
+		sqlExecutionService.executeSqlFile(file);
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
 }
