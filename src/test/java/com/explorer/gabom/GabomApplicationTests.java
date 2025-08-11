4114a58dcbdab2e96cb94f7153de647d7563bc70
dain <0901ekdls@gmail.com>
2025-08-12 04:43:57 +0900

    ci: 테스트코드오류+ @activeprofiles("test")

--- src/test/java/com/explorer/gabom/GabomApplicationTests.java
+++ src/test/java/com/explorer/gabom/GabomApplicationTests.java
@@ -2,9 +2,11 @@ package com.explorer.gabom;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class GabomApplicationTests {
+@ActiveProfiles("test")
+public class GabomApplicationTests {
 
     @Test
     void contextLoads() {
