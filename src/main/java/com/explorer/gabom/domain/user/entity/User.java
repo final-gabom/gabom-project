087fc155df3a96bdee23caa2e966df0cf4671f2c
gyogyo0212 <gyogyo0212@gamil.com>
2025-08-13 02:31:08 +0900

    feature: User.java
    - 빌더의 status ACTIVE 삭제
    
    resolves: #265

--- src/main/java/com/explorer/gabom/domain/user/entity/User.java
+++ src/main/java/com/explorer/gabom/domain/user/entity/User.java
@@ -1,5 +1,10 @@
 package com.explorer.gabom.domain.user.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.SQLDelete;
+
 import com.explorer.gabom.domain.address.entity.Address;
 import com.explorer.gabom.domain.file.entity.AttachmentFile;
 import com.explorer.gabom.domain.title.entity.Title;
@@ -8,14 +13,22 @@ import com.explorer.gabom.domain.user.type.UserRole;
 import com.explorer.gabom.domain.user.type.UserStatus;
 import com.explorer.gabom.global.entity.BaseTimeEntity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SQLDelete;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -24,96 +37,65 @@ import java.util.List;
 @Table(name = "users")
 public class User extends BaseTimeEntity {
 
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-	private final List<UserTitle> userTitles = new ArrayList<>();
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
-	@Column(unique = true, nullable = false)
-	private String email;
-
-	@Column(nullable = false)
-	private String password;
-
-	@Column(unique = true, nullable = false)
-	private String nickname;
-
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private UserRole userRole;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "profile_img_id")
-	private AttachmentFile profileImg;
-
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private UserStatus status;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "title_id")
-	private Title title;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "address_id", insertable = false, updatable = false)
-	private Address address;
-	@Column(name = "address_id")
-	private Long addressId;
-
-	@Column(nullable = false)
-	private Integer point;
-
-	@Column(nullable = false)
-	private Integer level;
-
-	@Column(nullable = false)
-	private Integer exp;
-
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<UserTitle> userTitles = new ArrayList<>();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true, nullable = false)
     private String email;
+
     @Column(nullable = false)
     private String password;
+
     @Column(unique = true, nullable = false)
     private String nickname;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRole userRole;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "profile_img_id")
     private AttachmentFile profileImg;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserStatus status;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "title_id")
     private Title title;
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "addressId", insertable = false, updatable = false)
+    @JoinColumn(name = "address_id", insertable = false, updatable = false)
     private Address address;
+    @Column(name = "address_id")
     private Long addressId;
+
     @Column(nullable = false)
     private Integer point;
+
     @Column(nullable = false)
     private Integer level;
+
     @Column(nullable = false)
     private Integer exp;
 
-	@Builder
-	public User(Long id, String email, String password, String nickname, UserRole userRole) {
-		this.id = id;
-		this.email = email;
-		this.password = password;
-		this.nickname = nickname;
-		this.userRole = userRole;
-		this.status = UserStatus.ACTIVE;
-		this.point = 0;
-		this.level = 1;
-		this.exp = 0;
-	}
+
+    @Builder
+    public User(Long id, String email, String password, String nickname, UserRole userRole, UserStatus status) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.userRole = userRole;
+        this.status = status;
+        this.point = 0;
+        this.level = 1;
+        this.exp = 0;
+    }
 
     public void addPoint(int point) {
         this.point += point;
