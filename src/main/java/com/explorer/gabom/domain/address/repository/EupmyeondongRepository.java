5c0c897de92769c6de3d6db3eb890cabb41f5195
Kimjieun <jieun8496@icloud.com>
2025-08-21 01:31:59 +0900

    feat: Place CSV Import 성능 개선 (#324)

--- src/main/java/com/explorer/gabom/domain/address/repository/EupmyeondongRepository.java
+++ src/main/java/com/explorer/gabom/domain/address/repository/EupmyeondongRepository.java
@@ -1,11 +1,16 @@
 package com.explorer.gabom.domain.address.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.explorer.gabom.domain.address.entity.Eupmyeondong;
 
 public interface EupmyeondongRepository extends JpaRepository<Eupmyeondong, String> {
 	boolean existsByEmdCd(String emdCd);
+
+	@Query("select e.emdCd from Eupmyeondong e")
+	List<String> findAllEmdCd();
 }
