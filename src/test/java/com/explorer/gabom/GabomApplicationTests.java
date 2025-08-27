7de06eb75babc1609b82d438821e0890068c576e
dain <0901ekdls@gmail.com>
2025-08-27 22:23:39 +0900

    test4

--- src/test/java/com/explorer/gabom/GabomApplicationTests.java
+++ src/test/java/com/explorer/gabom/GabomApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.explorer.gabom.domain.elasticsearch.indexer.PlaceSearchIndexer;
+
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 
 @SpringBootTest
@@ -13,6 +15,9 @@ class GabomApplicationTests {
 
     @MockBean
     ElasticsearchClient elasticsearchClient;
+
+    @MockBean
+    PlaceSearchIndexer placeSearchIndexer;
 
     @Test
     void contextLoads() {
